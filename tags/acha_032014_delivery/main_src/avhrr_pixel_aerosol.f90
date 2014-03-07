856ea6c03f0964896552ef90ada288eb9edecc39
wstraka <wstraka@98759d60-67cc-457f-8aaf-b3b51597e956>
2014-03-07 18:08:43 +0000

    ACHA delivered to AIT on March 2014

---
+++ tags/acha_032014_delivery/main_src/avhrr_pixel_aerosol.f90
@@ -0,0 +1,355 @@
+!$Id$
+!--------------------------------------------------------------------------------------
+! Clouds from AVHRR Extended (CLAVR-x) 1b PROCESSING SOFTWARE Version 5.3
+!
+! NAME: avhrr_pixel_aerosol.f90 (src)
+!       AEROSOL_PROPERTIES (program)
+!
+! PURPOSE: a module for pixel level aerosol properties from AVHRR
+!
+! DESCRIPTION: this is the single channel algorithm used in PATMOS for
+!              aerosol optical in ch1 over the ocean
+!
+! AUTHORS:
+!  Andrew Heidinger, Andrew.Heidinger@noaa.gov
+!  Lookup tables provided by A. Ignatov (02/2005)
+!
+! COPYRIGHT
+! THIS SOFTWARE AND ITS DOCUMENTATION ARE CONSIDERED TO BE IN THE PUBLIC
+! DOMAIN AND THUS ARE AVAILABLE FOR UNRESTRICTED PUBLIC USE. THEY ARE
+! FURNISHED "AS IS." THE AUTHORS, THE UNITED STATES GOVERNMENT, ITS
+! INSTRUMENTALITIES, OFFICERS, EMPLOYEES, AND AGENTS MAKE NO WARRANTY,
+! EXPRESS OR IMPLIED, AS TO THE USEFULNESS OF THE SOFTWARE AND
+! DOCUMENTATION FOR ANY PURPOSE. THEY ASSUME NO RESPONSIBILITY (1) FOR
+! THE USE OF THE SOFTWARE AND DOCUMENTATION; OR (2) TO PROVIDE TECHNICAL
+! SUPPORT TO USERS.
+!
+! NOTES:
+! Input and output to these routines is through public arrays
+! through the PIXEL_COMMON module. 
+!
+! Public routines used in this module:
+!  READ_AER_CH123A_REF_LUTS - read the lookup tables (LUTS)
+!  PIXEL_AER_RET_OCEAN - perform the estimation of optical depth
+!
+! Private routines used in this module:
+!  AER_RET - routine used in estimation aerosol optical depth.
+!
+! File I/O 
+! Logical units are opened and closed during LUT read.
+!
+! public variables used
+!  Ref_Ch1 - channel 1 relflectance (%)
+!  Ref_Ch2 - channel 2 reflectance (%)
+!  Ref_Ch6 - channel 3a reflectance (%)
+!  satzen - sensor zenith angle (deg)
+!  Solzen - solar zenith angle (deg)
+!  Relaz - relative azimuth angle (deg)
+!
+! output passed through PIXEL_COMMON
+! aot1 - channel 1 aerosol optical depth
+! aot2 - channel 2 aerosol optical depth
+! aot3a - channel 3a aerosol optical depth
+!
+! aerosol lookup table variables
+! tau_lut - optical depth used in tables - dimension (ntau)
+! Solzen_lut - solar zenith angles used in tables - dimension(nSolzen)
+! zen_lut - sensor zenith angles used in tables -dimension (nzen)
+! az_lut - relative azimuth angles used in tables - dimension(naz)
+! Ref_lut_Ch1_aer - ch1 reflectances (0-1) of lookup table - 
+!                    dimension(nSolzen,nzen,ntau,naz)
+! Ref_lut_Ch2_aer - ch2 reflectances (0-1) of lookup table
+! Ref_lut_Ch3a_aer - ch3a reflectances (0-1) of lookup table
+!
+! Note, aerosol product quality flags are computed elsewhere.
+!
+!--------------------------------------------------------------------------------------
+module AEROSOL_PROPERTIES
+ use PIXEL_COMMON
+ use CONSTANTS
+ use FILE_UTILITY
+ use NUMERICAL_ROUTINES
+ implicit none
+ private
+ public:: READ_AER_CH123A_REF_LUTS,&
+          PIXEL_AER_RET_OCEAN
+ private:: AER_RET
+
+!------------------------------------------------------------------------
+ integer, parameter, private:: nzen = 15, naz = 19, nSolzen = 15, ntau = 7
+
+!--- aerosol lut variables
+ real, dimension(ntau), save, private::tau_lut
+ real, dimension(nSolzen), save, private::Solzen_lut
+ real, dimension(nzen), save, private::zen_lut
+ real, dimension(naz), save, private::az_lut
+ real, save, public:: lambda_ref
+ real, save, private:: delta_Solzen,delta_zen,delta_az,delta_tau
+ real, dimension(nSolzen,nzen,ntau,naz), save, private::  Ref_lut_Ch1_aer, &
+                                                 Ref_lut_Ch2_aer,Ref_lut_Ch3a_aer
+
+!--- other module-wide variables
+ real, dimension(ntau), save, private::temp_Ref_lut
+
+ contains
+
+!----------------------------------------------------------------------------
+! grid_cell aerosol retrieval
+!
+! This uses linear in tau and angle interpolation - should improve this base
+! on scheme used for operational aerosol retrieval.
+!
+! temp_Ref_lut is a reflectance vector of dimension(ntau) that is already
+!         interpolated in the angular dimensions.
+!
+!----------------------------------------------------------------------------
+subroutine PIXEL_AER_RET_OCEAN(jmin,jmax)
+
+   integer, intent(in):: jmin, jmax
+
+   integer:: i,j,it,iu,iv,ifail
+   real(kind=real4):: v,t,u,missing_value
+ 
+   missing_value = -999.0
+   ifail = 0
+
+!--------------------------------------------------------------------------
+! loop over pixels in scanlines
+!--------------------------------------------------------------------------
+scan_loop: do j = jmin, jmax + jmin - 1
+
+pixel_loop:  do i = 1, num_pix
+
+!--- check for a bad pixel
+if (bad_pixel_mask(i,j) == sym%YES) then 
+    cycle
+endif
+
+!--- check for valid illumination and surface
+
+if ((Solzen(i,j) > 87.0) .or. (sfc_type(i,j) /= sym%WATER_SFC) .or. (snow(i,j) /= sym%NO_SNOW)) then
+
+   aot1(i,j) = missing_value_real4
+   aot2(i,j) = missing_value_real4
+   aot3a(i,j) = missing_value_real4
+
+
+ else
+
+  it = max(1,min(nSolzen-1,1+int( (Solzen(i,j)-Solzen_lut(1)) / (delta_Solzen))))
+  iu = max(1,min(nzen-1,1+int( (satzen(i,j)-zen_lut(1)) / (delta_zen))))
+  iv = max(1,min(naz-1,1+int( min(Relaz(i,j),360.0-Relaz(i,j)) / (delta_az)) ))
+  u = (satzen(i,j) - zen_lut(iu))/(zen_lut(iu+1)-zen_lut(iu))
+  v = (min(Relaz(i,j),360.0-Relaz(i,j)) - az_lut(iv))/(az_lut(iv+1)-az_lut(iv))
+  t =  (Solzen(i,j) - Solzen_lut(it))/(Solzen_lut(it+1)-Solzen_lut(it))
+
+  ifail = 0
+ 
+ 
+  !--  channel 1
+  temp_Ref_lut = (1.0-t)*(1.0-u)*(1.0-v)*Ref_lut_Ch1_aer(it,iu,:,iv) +  &
+                 (1.0-t)*(1.0-u)*(v)*Ref_lut_Ch1_aer(it,iu,:,iv+1) +  &
+                 (t)*(1.0-u)*(1.0-v)*Ref_lut_Ch1_aer(it+1,iu,:,iv) +  &
+                 (t)*(1.0-u)*(v)*Ref_lut_Ch1_aer(it+1,iu,:,iv+1) +  &
+                 (1.0-t)*(u)*(1.0-v)*Ref_lut_Ch1_aer(it,iu+1,:,iv) +  &
+                 (1.0-t)*(u)*(v)*Ref_lut_Ch1_aer(it,iu+1,:,iv+1) +  &
+                 (t)*(u)*(1.0-v)*Ref_lut_Ch1_aer(it+1,iu+1,:,iv) +  &
+                 (t)*(u)*(v)*Ref_lut_Ch1_aer(it+1,iu+1,:,iv+1)
+  call AER_RET(ch(1)%Ref_Toa(i,j)/100.0, aot1(i,j))
+
+  !--  channel 2
+  temp_Ref_lut = (1.0-t)*(1.0-u)*(1.0-v)*Ref_lut_Ch2_aer(it,iu,:,iv) +  &
+                 (1.0-t)*(1.0-u)*(v)*Ref_lut_Ch2_aer(it,iu,:,iv+1) +  &
+                 (t)*(1.0-u)*(1.0-v)*Ref_lut_Ch2_aer(it+1,iu,:,iv) +  &
+                 (t)*(1.0-u)*(v)*Ref_lut_Ch2_aer(it+1,iu,:,iv+1) +  &
+                 (1.0-t)*(u)*(1.0-v)*Ref_lut_Ch2_aer(it,iu+1,:,iv) +  &
+                 (1.0-t)*(u)*(v)*Ref_lut_Ch2_aer(it,iu+1,:,iv+1) +  &
+                 (t)*(u)*(1.0-v)*Ref_lut_Ch2_aer(it+1,iu+1,:,iv) +  &
+                 (t)*(u)*(v)*Ref_lut_Ch2_aer(it+1,iu+1,:,iv+1)
+  call AER_RET(ch(2)%Ref_Toa(i,j)/100.0, aot2(i,j))
+
+
+  if (Ch3a_On_AVHRR(j) == sym%YES) then
+   !--  channel 3a
+   temp_Ref_lut = (1.0-t)*(1.0-u)*(1.0-v)*Ref_lut_Ch3a_aer(it,iu,:,iv) +  &
+                 (1.0-t)*(1.0-u)*(v)*Ref_lut_Ch3a_aer(it,iu,:,iv+1) +  &
+                 (t)*(1.0-u)*(1.0-v)*Ref_lut_Ch3a_aer(it+1,iu,:,iv) +  &
+                 (t)*(1.0-u)*(v)*Ref_lut_Ch3a_aer(it+1,iu,:,iv+1) +  &
+                 (1.0-t)*(u)*(1.0-v)*Ref_lut_Ch3a_aer(it,iu+1,:,iv) +  &
+                 (1.0-t)*(u)*(v)*Ref_lut_Ch3a_aer(it,iu+1,:,iv+1) +  &
+                 (t)*(u)*(1.0-v)*Ref_lut_Ch3a_aer(it+1,iu+1,:,iv) +  &
+                 (t)*(u)*(v)*Ref_lut_Ch3a_aer(it+1,iu+1,:,iv+1)
+   call AER_RET(ch(6)%Ref_Toa(i,j)/100.0, aot3a(i,j))
+  endif
+
+ endif            !end of check for appropriate retrieval conditions
+
+ end do pixel_loop
+end do scan_loop
+ 
+end subroutine PIXEL_AER_RET_OCEAN
+
+!------------------------------------------------------------
+! Actual routine to estimate aerosol optical depth from LUT
+!------------------------------------------------------------
+subroutine AER_RET(y,aot)
+
+real, intent(in):: y
+real, intent(out):: aot
+integer:: is
+real:: s
+
+!--- linear interp
+  call LOCATE(temp_Ref_lut,ntau,y,is)
+  is = max(1,min(ntau-1,is))
+  s = (y - temp_Ref_lut(is))/(temp_Ref_lut(is+1) - temp_Ref_lut(is))
+  aot = (1.0-s)*tau_lut(is) + s*tau_lut(is+1) 
+end subroutine AER_RET
+
+!------------------------------------------------------------------------------
+! READ IN THE NESDIS OPERATIONAL LOOKUP TABLES FOR CH1, CH2 and CH3A OCEANIC AEROSOL
+!------------------------------------------------------------------------------
+ subroutine READ_AER_CH123A_REF_LUTS(ancil_data_dir,wmonum)
+  character (len=*), intent(in):: ancil_data_dir
+  integer, intent(in):: wmonum
+  character (len=128):: ch1_aer_lut_file,ch2_aer_lut_file,ch3a_aer_lut_file
+  character (len=128):: header,basename
+  integer:: iSolzen,izen,itau
+  integer:: nzen1,naz1,nSolzen1,ntau1
+  integer:: nzen2,naz2,nSolzen2,ntau2
+  integer:: nzen3,naz3,nSolzen3,ntau3
+  real(kind=real4):: cos_Solzen
+  integer:: ch1_lun
+  integer:: ch2_lun
+  integer:: ch3a_lun
+
+
+  if (wmonum == 708) then
+      basename = "Ch1F.lut.dat-NOAATN"
+  elseif (wmonum == 706) then
+      basename = "Ch1F.lut.dat-NOAA06"
+  elseif(wmonum == 707) then
+      basename = "Ch1F.lut.dat-NOAA07"
+  elseif(wmonum == 200) then
+      basename = "Ch1F.lut.dat-NOAA08"
+  elseif(wmonum == 201) then
+      basename = "Ch1F.lut.dat-NOAA09"
+  elseif(wmonum == 202) then
+      basename = "Ch1F.lut.dat-NOAA10"
+  elseif(wmonum == 203) then
+      basename = "Ch1F.lut.dat-NOAA11"
+  elseif(wmonum == 204) then
+      basename = "Ch1F.lut.dat-NOAA12"
+  elseif(wmonum == 205) then
+      basename = "Ch1F.lut.dat-NOAA14"
+  elseif(wmonum == 206) then
+      basename = "Ch1F.lut.dat-NOAA15"
+  elseif(wmonum == 207) then
+      basename = "Ch1F.lut.dat-NOAA16"
+  elseif(wmonum == 208) then
+      basename = "Ch1F.lut.dat-NOAA17"
+  elseif(wmonum == 209) then
+      basename = "Ch1F.lut.dat-NOAA18"
+  elseif(wmonum == 223) then
+      basename = "Ch1F.lut.dat-NOAA19"
+  elseif(wmonum == 4) then
+      basename = "Ch1F.lut.dat-MetOpA"
+  elseif(wmonum == 3) then
+      basename = "Ch1F.lut.dat-MetOpB"
+  elseif(wmonum == 5) then
+      basename = "Ch1F.lut.dat-MetOpC"
+  else
+      print *, "no aerosol lookup tables for satellite number = ", wmonum
+      print *, "stopping"
+      stop
+  endif
+
+  ch1_aer_lut_file = trim(ancil_data_dir)//"luts/aerosol/"//trim(basename)//"_1"
+  ch2_aer_lut_file = trim(ancil_data_dir)//"luts/aerosol/"//trim(basename)//"_2"
+  ch3a_aer_lut_file = trim(ancil_data_dir)//"luts/aerosol/"//trim(basename)//"_3"
+
+  if (wmonum >= 200 .and. wmonum <= 205) then
+    ch3a_aer_lut_file = trim(ancil_data_dir)//"luts/aerosol/"//"Ch1F.lut.dat-NOAA15_3"
+  endif
+  if (wmonum >= 706 .and. wmonum <= 708) then
+    ch3a_aer_lut_file = trim(ancil_data_dir)//"luts/aerosol/"//"Ch1F.lut.dat-NOAA15_3"
+  endif
+
+  ch1_lun = GET_LUN()
+  open(unit=ch1_lun,file=ch1_aer_lut_file,status="old",position="rewind",action="read")
+  ch2_lun = GET_LUN()
+  open(unit=ch2_lun,file=ch2_aer_lut_file,status="old",position="rewind",action="read")
+  ch3a_lun = GET_LUN()
+  open(unit=ch3a_lun,file=ch3a_aer_lut_file,status="old",position="rewind",action="read")
+
+   read(unit=ch1_lun,fmt="(4i5)") nzen1, naz1, nSolzen1,ntau1
+   read(unit=ch2_lun,fmt="(4i5)") nzen2, naz2, nSolzen2,ntau2
+   read(unit=ch3a_lun,fmt="(4i5)") nzen3, naz3, nSolzen3,ntau3
+
+   Ref_lut_Ch1_aer = 0.0
+   Ref_lut_Ch2_aer = 0.0
+   Ref_lut_Ch3a_aer = 0.0
+   tau_lut = 0.00
+   Solzen_lut = 0.00
+   zen_lut = 0.00
+   az_lut = 0.00
+   lambda_ref = 0.63
+   delta_tau = 0.15
+
+   temp_Ref_lut = 0.0
+
+   delta_Solzen = 90.0 / nSolzen
+   do iSolzen = 1,nSolzen
+    Solzen_lut(iSolzen) = (iSolzen-1)*delta_Solzen
+   enddo
+
+
+   read(unit=ch1_lun, fmt=*) tau_lut
+   read(unit=ch2_lun, fmt=*) tau_lut
+   read(unit=ch3a_lun, fmt=*) tau_lut
+
+   tau_lut = tau_lut * delta_tau
+
+   do iSolzen = 1,nSolzen
+       cos_Solzen = cos(Solzen_lut(iSolzen)*dtor)
+     do itau = 1, ntau
+       read(unit=ch1_lun,fmt="(a)") header
+       read(unit=ch2_lun,fmt="(a)") header
+       read(unit=ch3a_lun,fmt="(a)") header
+
+       read(unit=ch1_lun,fmt="(10f6.0,/,9f6.0)") az_lut
+       read(unit=ch2_lun,fmt="(10f6.0,/,9f6.0)") az_lut
+       read(unit=ch3a_lun,fmt="(10f6.0,/,9f6.0)") az_lut
+      
+       do izen = 1, nzen
+        read(unit=ch1_lun,fmt=*) zen_lut(izen), Ref_lut_Ch1_aer(iSolzen,izen,itau,1:5)
+        read(unit=ch1_lun,fmt=*) Ref_lut_Ch1_aer(iSolzen,izen,itau,6:10)
+        read(unit=ch1_lun,fmt=*) Ref_lut_Ch1_aer(iSolzen,izen,itau,11:15)
+        read(unit=ch1_lun,fmt=*) Ref_lut_Ch1_aer(iSolzen,izen,itau,16:19)
+
+        read(unit=ch2_lun,fmt=*) zen_lut(izen), Ref_lut_Ch2_aer(iSolzen,izen,itau,1:5)
+        read(unit=ch2_lun,fmt=*) Ref_lut_Ch2_aer(iSolzen,izen,itau,6:10)
+        read(unit=ch2_lun,fmt=*) Ref_lut_Ch2_aer(iSolzen,izen,itau,11:15)
+        read(unit=ch2_lun,fmt=*) Ref_lut_Ch2_aer(iSolzen,izen,itau,16:19)
+
+        read(unit=ch3a_lun,fmt=*) zen_lut(izen), Ref_lut_Ch3a_aer(iSolzen,izen,itau,1:5)
+        read(unit=ch3a_lun,fmt=*) Ref_lut_Ch3a_aer(iSolzen,izen,itau,6:10)
+        read(unit=ch3a_lun,fmt=*) Ref_lut_Ch3a_aer(iSolzen,izen,itau,11:15)
+        read(unit=ch3a_lun,fmt=*) Ref_lut_Ch3a_aer(iSolzen,izen,itau,16:19)
+
+       enddo 
+     enddo
+     Ref_lut_Ch1_aer(iSolzen,:,:,:) = Ref_lut_Ch1_aer(iSolzen,:,:,:)/cos_Solzen
+     Ref_lut_Ch2_aer(iSolzen,:,:,:) = Ref_lut_Ch2_aer(iSolzen,:,:,:)/cos_Solzen
+     Ref_lut_Ch3a_aer(iSolzen,:,:,:) = Ref_lut_Ch3a_aer(iSolzen,:,:,:)/cos_Solzen
+   enddo
+
+  close(unit=ch1_lun)
+  close(unit=ch2_lun)
+  close(unit=ch3a_lun)
+
+   delta_zen = zen_lut(2) - zen_lut(1)
+   delta_az = az_lut(2) - az_lut(1)
+ end subroutine READ_AER_CH123A_REF_LUTS
+
+end module AEROSOL_PROPERTIES
---
+++ tags/acha_032014_delivery/main_src/cloud_height_routines.f90
@@ -0,0 +1,321 @@
+!$Id$
+!--------------------------------------------------------------------------------------
+! Clouds from AVHRR Extended (CLAVR-x) 1b PROCESSING SOFTWARE Version 5.3
+!
+! NAME: cloud_height_routines.f90 (src)
+!       CLOUD_HEIGHT_ROUTINES (program)
+!
+! PURPOSE: This module houses the routines associated with...
+!          Cloud Algorithms other than ACHA - AWG Cloud Height Algorithm
+!
+! DESCRIPTION:
+!
+! AUTHORS:
+!  Andrew Heidinger, Andrew.Heidinger@noaa.gov
+!
+! COPYRIGHT
+! THIS SOFTWARE AND ITS DOCUMENTATION ARE CONSIDERED TO BE IN THE PUBLIC
+! DOMAIN AND THUS ARE AVAILABLE FOR UNRESTRICTED PUBLIC USE. THEY ARE
+! FURNISHED "AS IS." THE AUTHORS, THE UNITED STATES GOVERNMENT, ITS
+! INSTRUMENTALITIES, OFFICERS, EMPLOYEES, AND AGENTS MAKE NO WARRANTY,
+! EXPRESS OR IMPLIED, AS TO THE USEFULNESS OF THE SOFTWARE AND
+! DOCUMENTATION FOR ANY PURPOSE. THEY ASSUME NO RESPONSIBILITY (1) FOR
+! THE USE OF THE SOFTWARE AND DOCUMENTATION; OR (2) TO PROVIDE TECHNICAL
+! SUPPORT TO USERS.
+!
+!--------------------------------------------------------------------------------------
+module CLOUD_HEIGHT_ROUTINES
+  use CONSTANTS
+  use PIXEL_COMMON
+  use NWP_COMMON
+  use RTM_COMMON
+  use NUMERICAL_ROUTINES
+
+  implicit none
+
+  PUBLIC::  OPAQUE_CLOUD_HEIGHT   
+  PUBLIC::  COMPUTE_CLOUD_TOP_LEVEL_NWP_WIND
+  PUBLIC::  COMPUTE_ALTITUDE_FROM_PRESSURE
+
+
+  !--- include parameters for each system here
+  INTEGER(KIND=INT4), PRIVATE, PARAMETER :: Chan_Idx_375um = 20  !channel number for 3.75 micron
+  INTEGER(KIND=INT4), PRIVATE, PARAMETER :: Chan_Idx_67um = 27   !channel number for 6.7 micron
+  INTEGER(KIND=INT4), PRIVATE, PARAMETER :: Chan_Idx_73um = 28   !channel number for 7.3 micron
+  INTEGER(KIND=INT4), PRIVATE, PARAMETER :: Chan_Idx_85um = 29   !channel number for 8.5 micron
+  INTEGER(KIND=INT4), PRIVATE, PARAMETER :: Chan_Idx_11um = 31   !channel number for 11 micron
+  INTEGER(KIND=INT4), PRIVATE, PARAMETER :: Chan_Idx_12um = 32   !channel number for 12 micron
+  INTEGER(KIND=INT4), PRIVATE, PARAMETER :: Chan_Idx_133um = 33  !channel number for 13.3 micron
+  REAL(KIND=REAL4), PARAMETER, PRIVATE:: Beta_Ap_Water = 1.3
+  REAL(KIND=REAL4), PARAMETER, PRIVATE:: Beta_Ap_Ice = 1.06
+  REAL(KIND=REAL4), PARAMETER, PRIVATE:: Ice_Temperature_Min = 243.0
+  REAL(KIND=REAL4), PARAMETER, PRIVATE:: Ice_Temperature_Max = 263.0
+
+  REAL(KIND=REAL4), PARAMETER, PRIVATE:: Rad_Window_Thresh = 2.0
+  REAL(KIND=REAL4), PARAMETER, PRIVATE:: Rad_H2O_Thresh = 0.25
+  REAL, PRIVATE, PARAMETER:: Bt_Ch27_Ch31_Covar_Cirrus_Thresh = 0.5
+  REAL, PRIVATE, PARAMETER:: Bt_Ch31_Btd_Ch31_Ch32_Covar_Cirrus_Thresh = -1.0
+  REAL, PRIVATE, PARAMETER:: Beta_11um_85um_Ice_Thresh = 1.10
+  REAL, PRIVATE, PARAMETER:: Beta_11um_12um_Overlap_Thresh = 0.95
+  REAL, PRIVATE, PARAMETER:: Beta_11um_133um_Overlap_Thresh = 0.70
+  REAL, PRIVATE, PARAMETER:: Emiss_67um_Cirrus_Thresh = 0.05
+  REAL, PRIVATE, PARAMETER:: Emiss_133um_Cirrus_Thresh = 0.02
+
+  CONTAINS
+
+!----------------------------------------------------------------------
+! Cloud Height for AVHRR/1.  Use precomputed opaque solutions
+! for AVHRR/1 sensors.  Set quality flag to degraded (1)
+!----------------------------------------------------------------------
+subroutine  OPAQUE_CLOUD_HEIGHT(Line_Idx_min,Num_Lines)
+                              
+  INTEGER, INTENT(IN):: Line_Idx_min
+  INTEGER, INTENT(IN):: Num_Lines
+
+  INTEGER:: Elem_Idx
+  INTEGER:: Line_Idx
+  INTEGER:: Number_Of_Elements
+
+  !--- intialize local variables using global variables
+  Number_Of_Elements = num_pix
+
+  !--- initialize output
+  Tc_Acha =  Missing_Value_Real4
+  Ec_Acha =  Missing_Value_Real4
+  Beta_Acha =  Missing_Value_Real4
+  Pc_Acha =  Missing_Value_Real4
+  Zc_Acha =  Missing_Value_Real4
+  Acha_Quality_Flag = 0
+  Acha_OE_Quality_Flags = 0
+  Cld_Layer_Acha = 0
+
+  !--------------------------------------------------------------------------
+  ! loop over pixels in scanlines
+  !--------------------------------------------------------------------------
+  Line_loop: do Line_Idx = Line_Idx_min,Num_Lines + Line_Idx_min - 1
+
+    Element_loop:   do Elem_Idx = 1, Number_Of_Elements
+
+    !--- check for a bad pixel
+    if (Bad_pixel_mask(Elem_Idx,Line_Idx) == sym%YES) then
+          cycle
+    endif
+
+    if (Tc_Opaque_Cloud(Elem_Idx,Line_Idx) /= Missing_Value_Real4) then
+       Pc_Acha(Elem_Idx,Line_Idx) = Pc_Opaque_Cloud(Elem_Idx,Line_Idx)
+       Tc_Acha(Elem_Idx,Line_Idx) = Tc_Opaque_Cloud(Elem_Idx,Line_Idx)
+       Zc_Acha(Elem_Idx,Line_Idx) = Zc_Opaque_Cloud(Elem_Idx,Line_Idx)
+       Ec_Acha(Elem_Idx,Line_Idx) = 1.0
+       Acha_Quality_Flag(Elem_Idx,Line_Idx) = 1
+
+       !------- determine cloud layer based on pressure
+       if (Pc_Acha(Elem_Idx,Line_Idx) <= 440.0) then
+         Cld_Layer_Acha(Elem_Idx,Line_Idx) = 3
+       elseif (Pc_Acha(Elem_Idx,Line_Idx) < 680.0) then
+         Cld_Layer_Acha(Elem_Idx,Line_Idx) = 2
+       else
+         Cld_Layer_Acha(Elem_Idx,Line_Idx) = 1
+       endif
+
+       !----- set beta passed on temp
+       if (Tc_Acha(Elem_Idx,Line_Idx) < 260.0) then
+           Beta_Acha(Elem_Idx,Line_Idx) = Beta_Ap_Ice
+       else
+           Beta_Acha(Elem_Idx,Line_Idx) = Beta_Ap_Water
+       endif
+
+    endif
+
+    end do Element_loop
+
+  end do Line_loop
+end subroutine OPAQUE_CLOUD_HEIGHT
+
+!----------------------------------------------------------------------
+! routine to interpolate nwp winds at cloud height
+!----------------------------------------------------------------------
+subroutine COMPUTE_CLOUD_TOP_LEVEL_NWP_WIND(Line_Idx_Min,Num_Lines)
+
+  INTEGER, INTENT(IN):: Line_Idx_Min
+  INTEGER, INTENT(IN):: Num_Lines
+  INTEGER:: Num_Elem
+  INTEGER:: Elem_Idx
+  INTEGER:: Line_Idx
+  INTEGER:: Line_Start
+  INTEGER:: Line_End
+  INTEGER:: Inwp
+  INTEGER:: Jnwp
+  INTEGER:: Ivza
+  INTEGER:: Level
+  REAL:: Tc_Temp
+  REAL:: Zc_Temp
+  REAL:: U_Wnd_Temp
+  REAL:: V_Wnd_Temp
+
+  !--- save elements
+  Line_Start = Line_Idx_Min 
+  Line_End = Line_Start + Num_Lines - 1
+  Num_Elem = Num_Pix      !Num_Pix is a global variable
+
+  !--- initialize
+  Wnd_Spd_Cld_Top_Nwp_Pix = Missing_Value_Real4
+  Wnd_Dir_Cld_Top_Nwp_Pix = Missing_Value_Real4
+
+  !----------------------------------------------------------
+  ! loop through segment
+  !----------------------------------------------------------
+  Line_Loop_1: do Line_Idx = Line_Start, Line_End
+  Element_Loop_1: do Elem_Idx = 1, Num_Elem
+
+     !--- save indices
+     Inwp = I_Nwp(Elem_Idx,Line_Idx)
+     Jnwp = J_Nwp(Elem_Idx,Line_Idx)
+     Ivza = Zen_Idx_Rtm(Elem_Idx,Line_Idx)
+
+     !--- skip bad pixels
+     if (Bad_Pixel_Mask(Elem_Idx,Line_Idx) == sym%YES) cycle
+
+     !-- check if indices are valid
+     if (Inwp < 0 .or. Jnwp < 0) cycle
+
+     !-- check if cld-top pressure is valid
+     if (Pc_Acha(Elem_Idx,Line_Idx) == Missing_Value_Real4) cycle
+
+     !--- determine Level in profiles 
+     !--- note, wind profiles are at the native nwp resolution
+     call KNOWING_P_COMPUTE_T_Z_NWP(Inwp,Jnwp, &
+                                    Pc_Acha(Elem_Idx,Line_Idx), &
+                                    Tc_Temp,Zc_Temp,Level)
+
+     !--- assign u and v winds
+     U_Wnd_Temp = U_Wnd_Prof_Nwp(Level,Inwp,Jnwp) 
+     V_Wnd_Temp = V_Wnd_Prof_Nwp(Level,Inwp,Jnwp) 
+
+     !-- determine speed and direction
+     Wnd_Spd_Cld_Top_Nwp_Pix(Elem_Idx,Line_Idx) = wind_speed(U_Wnd_Temp, V_Wnd_Temp)
+     Wnd_Dir_Cld_Top_Nwp_Pix(Elem_Idx,Line_Idx) = wind_direction(U_Wnd_Temp, V_Wnd_Temp)
+
+  END DO Element_Loop_1
+  END DO Line_Loop_1
+
+end subroutine COMPUTE_CLOUD_TOP_LEVEL_NWP_WIND
+
+!----------------------------------------------------------------------
+! routine to interpolate pressure to flight level altitude.
+!----------------------------------------------------------------------
+subroutine COMPUTE_ALTITUDE_FROM_PRESSURE(Line_Idx_Min,Num_Lines)
+
+!--- Based on Sarah Monette calculations for HS3.  Her calculation is based on:
+!--- http://psas.pdx.edu/RocketScience/PressureAltitude_Derived.pdf.
+
+  INTEGER, INTENT(IN):: Line_Idx_Min
+  INTEGER, INTENT(IN):: Num_Lines
+  INTEGER:: Num_Elem
+  INTEGER:: Elem_Idx
+  INTEGER:: Line_Idx
+  INTEGER:: Line_Start
+  INTEGER:: Line_End
+  INTEGER:: Inwp
+  INTEGER:: Jnwp
+  REAL:: Pc_Temp
+  REAL:: Tc_Temp
+  REAL:: Alt_Temp
+
+  !--- Constants from Sarah Monette
+  REAL, PARAMETER :: PW1 = 227.9 ! hPa
+  REAL, PARAMETER :: PW2 = 56.89 ! hPa
+  REAL, PARAMETER :: PW3 = 11.01 ! hPa
+  REAL, PARAMETER :: P0 = 1013.25 ! hPa
+  REAL, PARAMETER :: LR_OVER_G = 0.190263
+  REAL, PARAMETER :: Z0 = 145422.16 ! feet
+  REAL, PARAMETER :: LN_1 = -20859.0
+  REAL, PARAMETER :: LN_2 = 149255.0
+  REAL, PARAMETER :: PN_4 = 0.000470034
+  REAL, PARAMETER :: PN_3 = -0.364267
+  REAL, PARAMETER :: PN_2 = 47.5627
+  REAL, PARAMETER :: PN_1 = -2647.45
+  REAL, PARAMETER :: PN_0 = 123842.0
+
+  !--- save elements
+  Line_Start = Line_Idx_Min
+  Line_End = Line_Start + Num_Lines - 1
+  Num_Elem = Num_Pix      !Num_Pix is a global variable
+
+  !--- initialize
+  Alt_Acha = Missing_Value_Real4
+
+  !----------------------------------------------------------
+  ! loop through segment
+  !----------------------------------------------------------
+  Line_Loop_1: do Line_Idx = Line_Start, Line_End
+  Element_Loop_1: do Elem_Idx = 1, Num_Elem
+
+     !--- Initialize temporary value each time.
+     Pc_Temp = Missing_Value_Real4
+     Tc_Temp = Missing_Value_Real4
+     Alt_Temp = Missing_Value_Real4
+
+     !--- save indices
+     Inwp = I_Nwp(Elem_Idx,Line_Idx)
+     Jnwp = J_Nwp(Elem_Idx,Line_Idx)
+
+     !--- skip bad pixels
+     if (Bad_Pixel_Mask(Elem_Idx,Line_Idx) == sym%YES) cycle
+
+     !--- check if indices are valid
+     !--- stw May not need this.
+     if (Inwp < 0 .or. Jnwp < 0) cycle
+
+     !--- check if cld-top pressure is valid
+     if (Pc_Acha(Elem_Idx,Line_Idx) == Missing_Value_Real4) cycle
+
+     !--- Place valid pressure in temp variable for readability in the
+     !--- calculations below.
+     Pc_Temp = Pc_Acha(Elem_Idx,Line_Idx)
+     Tc_Temp = Tc_Acha(Elem_Idx,Line_Idx)
+
+     !IF (Pc_Temp < PW3) THEN
+     !  print*,"Pc_Temp : ", Pc_Temp, Tc_Temp
+     !ENDIF
+
+     !--- calculated altitude, in feet, from pressure.
+     !--- 1st pivot point is directly from the pressure to
+     !--- altitude from above reference.
+     IF (Pc_Temp > PW1) THEN
+       Alt_Temp = (1.0 - (Pc_Temp/P0)**LR_OVER_G) * Z0
+!stw       print*,"Pc_Temp/Alt_Temp : ", Pc_Temp, Alt_Temp
+     ENDIF
+
+     !--- 2nd pivot point was modeled best with a natural log
+     !--- fit.  From Sarah Monette.
+     IF (Pc_Temp <= PW1 .AND. Pc_Temp >= PW2) THEN
+       Alt_Temp = LN_1 * LOG(Pc_Temp) + LN_2
+!stw       print*,"Pc_Temp/Alt_Temp : ", Pc_Temp, Alt_Temp
+     ENDIF
+
+     !--- 3rd pivot point. Modeled best with a polynomial
+     !--- fit from Sarah Monette.
+     IF (Pc_Temp < PW2 .AND. Pc_Temp >= PW3) THEN
+       Alt_Temp = (PN_4*Pc_Temp**4) + (PN_3*Pc_Temp**3) + (PN_2*Pc_Temp**2) + &
+                  (PN_1*Pc_Temp) + PN_0
+!stw       print*,"Pc_Temp/Alt_Temp : ", Pc_Temp, Alt_Temp
+     ENDIF
+
+     IF (Pc_Temp < PW3) THEN
+       Alt_Temp = Missing_Value_Real4
+!stw       print*,"Pc_Temp/Alt_Temp : ", Pc_Temp, Alt_Temp
+     ENDIF
+
+     !--- Assign final altitude, in feet, to the level2 array.
+     Alt_Acha(Elem_Idx,Line_Idx) = Alt_Temp
+
+  END DO Element_Loop_1
+  END DO Line_Loop_1
+
+end subroutine COMPUTE_ALTITUDE_FROM_PRESSURE
+
+!----------------------------------------------------------------------
+! End of Module
+!----------------------------------------------------------------------
+
+end module CLOUD_HEIGHT_ROUTINES

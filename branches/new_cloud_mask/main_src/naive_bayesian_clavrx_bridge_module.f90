64097b9c7b101cf7c0aa2a11f313364a3e73cc4c
awalther <awalther@98759d60-67cc-457f-8aaf-b3b51597e956>
2014-04-09 18:54:09 +0000

    add comments to naive_bayesian_clavrx_bridge

--- branches/new_cloud_mask/main_src/naive_bayesian_clavrx_bridge_module.f90
+++ branches/new_cloud_mask/main_src/naive_bayesian_clavrx_bridge_module.f90
@@ -28,6 +28,54 @@
 !  HISTORY:
 !      2014/04/06:    new interface (AW)
 !
+!  GLOBAL VARIABLES:
+!
+!    FROM PIXEL_COMMON:
+!      1. work as input
+!        1.1 configuration
+!             Chan_On_Flag_Default                       integer (42)
+!             ancil_data_dir                             chacarcter
+!             Bayesian_Cloud_Mask_Name                   chacarcter
+!             num_pix                                    integer
+!             num_scans_read                             integer
+!        1.2 geo data:
+!              lon                                       real (:,:)
+!              lat                                       real (:,:)                        
+!              solzen                                    real (:,:)
+!              scatangle                                 real (:,:)
+!              airmass                                   real (:,:)
+!              glint_mask                                integer (:,:)
+!        1.3 surface 
+!              land                                      integer (:,:)
+!              coast                                     integer (:,:)
+!              snow                                      integer (:,:)
+!              zsfc                                      real (:,:)
+!        1.4 rtm 
+!              bt_ch31_lrc                               real (:,:)
+!              bt_ch31_max_3x3                           real (:,:)
+!              bt_Ch31_Std_3x3                           real (:,:)
+!              Ems_Ch20_Clear_Solar_Rtm                  real (:,:)
+!              ems_ch20_median_3x3                       real (:,:)
+!        1.5 observations
+!              ch                                        type ( observations )  
+!              Ref_Ch1_Std_3x3                           real (:,:)
+!              Ref_Ch1_Min_3x3                           real (:,:)
+!       
+!         2. work as output  
+!              Posterior_Cld_Probability                 real (:,:) 
+!              Bayes_Mask_Sfc_Type_Global                integer ( 3,:,:)
+!              Cld_Test_Vector_Packed                    integer ( 7,:,:)
+!              cld_mask                                  integer (:,:) 
+!           
+!     FROM CONSTANTS:
+!            sym                                         type ( symbol_struct ) 
+!
+!     NAIVE_BAYESIAN_CLOUD_MASK_MODULE
+!             cloud_mask_naive_bayes                     subroutine
+!                  subroutine which retrieves cloud probability
+!             cloud_mask_input_type                      type definition
+!                   structure definition of cloud_mask_naive_bayes retrieval input  
+!
 !--------------------------------------------------------------------------------------
 module naive_bayesian_clavrx_bridge_module
 
@@ -37,9 +85,38 @@ module naive_bayesian_clavrx_bridge_module
    use CONSTANTS , only: &
       & sym
       
-   use PIXEL_COMMON
-       
-   use CLOUD_MASK_MOD, only : &
+   use PIXEL_COMMON, only: &
+        lon &
+      , lat &
+      , solzen &
+      , airmass &
+      , scatangle &
+      , glint_mask &
+      , land &
+      , coast &
+      , snow &
+      , zsfc &
+      , bt_ch31_lrc &
+      , bt_ch31_max_3x3 &
+      , Bt_Ch31_Std_3x3 &
+      , ems_Ch20_Clear_Solar_Rtm &
+      , ems_ch20_median_3x3 &
+      , ch &
+      , Ref_Ch1_Std_3x3 &
+      , Ref_Ch1_Min_3x3 &
+      , Chan_On_Flag_Default &
+      , Posterior_Cld_Probability &
+      , Bayes_Mask_Sfc_Type_Global &
+      , Cld_Test_Vector_Packed &
+      , num_pix &
+      , num_scans_read &
+      , cld_mask &
+      , ancil_data_dir &
+      , Bayesian_Cloud_Mask_Name &
+     
+      
+             
+   use NAIVE_BAYESIAN_CLOUD_MASK_MODULE , only : &
       &   cloud_mask_naive_bayes &
       & , cloud_mask_input_type
 
@@ -57,71 +134,75 @@ contains
  
       implicit none
       
-      character (len=120):: Naive_Bayes_File_Name
-      type ( cloud_mask_input_type ) :: inp
-      integer :: info_flags(7)
+     
+      type ( cloud_mask_input_type ) :: mask_inp
+      ! cloud mask information mask 7 bytes ( 56 bits)    
+      integer :: info_flags ( 7 )
       integer :: i , j 
       real :: erg
            
-    ! -----------      
+    ! -----------    loop over pixels -----   
       line_loop: do i = 1, num_pix
          elem_loop: do  j = 1,num_scans_read
             
             if ( land (i,j) < 0 ) cycle
             
             
-            inp % bayesian_mask_classifier = trim(Ancil_Data_Dir)//'/naive_bayes_mask/'//trim(Bayesian_Cloud_Mask_Name) 
+            mask_inp % bayesian_mask_classifier = trim(Ancil_Data_Dir)//'/naive_bayes_mask/'//trim(Bayesian_Cloud_Mask_Name) 
             
-            inp % geo % lat         = lat ( i,j)            
-            inp % geo % lon         = lon ( i,j)
-            inp % geo % sol_zen     = solzen ( i,j)
-            inp % geo % airmass     = airmass ( i,j)
-            inp % geo % scat_angle  = scatangle ( i,j)
-            inp % geo % glint       = glint_mask ( i ,j )
+            mask_inp % geo % lat         = lat ( i,j)            
+            mask_inp % geo % lon         = lon ( i,j)
+            mask_inp % geo % sol_zen     = solzen ( i,j)
+            mask_inp % geo % airmass     = airmass ( i,j)
+            mask_inp % geo % scat_angle  = scatangle ( i,j)
+            mask_inp % geo % glint       = glint_mask ( i ,j )
    
-            inp % sfc % land_class  = land ( i , j )
-            inp % sfc % coast_mask  = coast ( i , j ) 
-            inp % sfc % snow_class  = snow( i , j )
-            inp % sfc % dem         = zsfc  ( i , j )
+            mask_inp % sfc % land_class  = land ( i , j )
+            mask_inp % sfc % coast_mask  = coast ( i , j ) 
+            mask_inp % sfc % snow_class  = snow( i , j )
+            mask_inp % sfc % dem         = zsfc  ( i , j )
          
-            inp % rtm % bt_ch31_lrc     =  Bt_Ch31_LRC ( i , j )
-            inp % rtm % bt_ch31_3x3_max = Bt_Ch31_Max_3x3 ( i , j )
-            inp % rtm % bt_ch31_3x3_std = Bt_Ch31_Std_3x3 ( i , j )
+            mask_inp % rtm % bt_ch31_lrc     =  Bt_Ch31_LRC ( i , j )
+            mask_inp % rtm % bt_ch31_3x3_max = Bt_Ch31_Max_3x3 ( i , j )
+            mask_inp % rtm % bt_ch31_3x3_std = Bt_Ch31_Std_3x3 ( i , j )
          
         
-            inp % rtm % emis_ch31_tropo = ch(31) % emiss_tropo (i,j)
-            inp % rtm % emis_ch32_tropo = ch(31) % emiss_tropo (i,j)
-            inp % rtm % emis_ch20_clear = Ems_Ch20_Clear_Solar_Rtm( i , j ) 
-            inp % rtm % bt_ch31_atm_sfc = ch(31)%Bt_Toa_Clear( i , j ) 
-            inp % rtm % bt_ch32_atm_sfc = ch(32)%Bt_Toa_Clear( i , j ) 
-            inp % rtm % ref_ch1_clear   = ch(1) % Ref_toa_clear (i,j)
-   
-            inp % sat % bt_ch20             = ch(20) % bt_toa ( i , j )
-            !inp % sat % bt_ch27             = sat % chn(27) % bt ( i , j )
-            inp % sat % bt_ch29             = ch(29) % bt_toa ( i , j )
-            inp % sat % bt_ch31             = ch(31) % bt_toa ( i , j )
-            inp % sat % bt_ch32             = ch(32) % bt_toa ( i , j )
+            mask_inp % rtm % emis_ch31_tropo = ch(31) % emiss_tropo (i,j)
+            mask_inp % rtm % emis_ch32_tropo = ch(32) % emiss_tropo (i,j)
+            mask_inp % rtm % emis_ch20_clear = Ems_Ch20_Clear_Solar_Rtm( i , j ) 
+            mask_inp % rtm % bt_ch31_atm_sfc = ch(31)%Bt_Toa_Clear( i , j ) 
+            mask_inp % rtm % bt_ch32_atm_sfc = ch(32)%Bt_Toa_Clear( i , j ) 
+            mask_inp % rtm % ref_ch1_clear   = ch(1) % Ref_toa_clear (i,j)
+  
+            mask_inp % sat % bt_ch20             = ch(20) % bt_toa ( i , j )
+            !mask_inp % sat % bt_ch27             = sat % chn(27) % bt ( i , j )
+            mask_inp % sat % bt_ch29             = ch(29) % bt_toa ( i , j )
+            mask_inp % sat % bt_ch31             = ch(31) % bt_toa ( i , j )
+            mask_inp % sat % bt_ch32             = ch(32) % bt_toa ( i , j )
 
-            inp % sat % ref_ch1             = ch(1) % ref_toa ( i , j )
+            mask_inp % sat % ref_ch1             = ch(1) % ref_toa ( i , j )
         
-            inp % sat % ref_ch2             = ch(2) % ref_toa ( i , j )
+            mask_inp % sat % ref_ch2             = ch(2) % ref_toa ( i , j )
         
-            inp % sat % ref_ch6             = ch(6) % ref_toa ( i , j )
+            mask_inp % sat % ref_ch6             = ch(6) % ref_toa ( i , j )
          
-            inp % sat % ref_ch26            = ch(26) % ref_toa ( i , j )
+            mask_inp % sat % ref_ch26            = ch(26) % ref_toa ( i , j )
         
-            inp % sat % emis_ch20_3x3_mean  = ems_ch20_median_3x3 ( i , j )
+            mask_inp % sat % emis_ch20_3x3_mean  = ems_ch20_median_3x3 ( i , j )
         
-            inp % sat % ref_ch1_3x3_std     = Ref_Ch1_Std_3x3(i,j)
-            inp % sat % ref_ch1_3x3_min     = Ref_Ch1_Min_3x3(i,j)
+            mask_inp % sat % ref_ch1_3x3_std     = Ref_Ch1_Std_3x3(i,j)
+            mask_inp % sat % ref_ch1_3x3_min     = Ref_Ch1_Min_3x3(i,j)
                        
-            inp % sat % chan_on ( 1:40) = Chan_On_Flag_Default(1:40) == 1
-             
-            call cloud_mask_naive_bayes ( inp, erg , info_flags )
+            mask_inp % sat % chan_on ( 1:40)     = Chan_On_Flag_Default(1:40) == 1
+            
+            
+           
+            call cloud_mask_naive_bayes ( mask_inp, Posterior_Cld_Probability ( i , j ) , info_flags )
          
-            Posterior_Cld_Probability ( i , j ) = erg
-            Bayes_Mask_Sfc_Type_Global (  i , j) = ibits ( info_flags (3) , 0, 3 ) 
-            Cld_Test_Vector_Packed ( : , i , j ) = info_flags
+              
+            Bayes_Mask_Sfc_Type_Global (  i , j ) = ibits ( info_flags (3) , 0, 3 ) 
+            Cld_Test_Vector_Packed ( : , i , j )  = info_flags
+            
          end do elem_loop
       end do   line_loop
       

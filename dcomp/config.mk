ea7efc11bfb68d39fc3334ee36735e1d009d087e
awalther <awalther@98759d60-67cc-457f-8aaf-b3b51597e956>
2014-06-12 15:01:29 +0000

    makes dcomp gfortran safe

--- dcomp/config.mk
+++ dcomp/config.mk
@@ -1,7 +1,7 @@
-# This file was generated on Thu Jun  5 12:17:00 2014 by awalther with the following command-line arguments: -hdf5root=/Users/awalther/lib/hdf5/ -with-ifort -hdflib=/Users/awalther/lib/hdf4//lib.
-# System info: Darwin luna.ssec.wisc.edu 13.2.0 Darwin Kernel Version 13.2.0: Thu Apr 17 23:03:13 PDT 2014; root:xnu-2422.100.13~1/RELEASE_X86_64 x86_64
+# This file was generated on Thu Jun 12 09:55:22 2014 by awalther with the following command-line arguments: -hdf5root=/home/awalther/lib/hdf5/ -with-ifort -hdflib=/home/awalther/lib/hdf4//lib.
+# System info: Linux odin.ssec.wisc.edu 2.6.18-53.1.4.el5 #1 SMP Wed Nov 14 10:37:27 EST 2007 x86_64 x86_64 x86_64 GNU/Linux
 fc = ifort
-fflags = -O2 -assume byterecl
-hdflibs = -L/Users/awalther/lib/hdf4//lib -lmfhdf -ldf -ljpeg -lz
-hdf5libs = -I/Users/awalther/lib/hdf5/include/ -L/Users/awalther/lib/hdf5/lib/
+fflags = -O2 
+hdflibs = -L/home/awalther/lib/hdf4//lib -lmfhdf -ldf -ljpeg -lz
+hdf5libs = -I/home/awalther/lib/hdf5/include/ -L/home/awalther/lib/hdf5/lib/
 hdf5links = -lhdf5_fortran -lhdf5 -lz

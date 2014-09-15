60ac5f197ea4a03af14a1c1a76cdedea558e4ac6
heidinger <heidinger@98759d60-67cc-457f-8aaf-b3b51597e956>
2014-09-15 18:02:56 +0000

    added new vars

--- main_src/scaling_params.f90
+++ main_src/scaling_params.f90
@@ -109,14 +109,17 @@ module SCALING_PARAMETERS
  real, parameter, public:: Min_Lon = -180, Max_Lon = 180.0
  real, parameter, public:: Min_Zc = 0.0, Max_Zc = 20000.0
  real, parameter, public:: Min_Zc_Uncer = 0.0, Max_Zc_Uncer = 10000.0
- real, parameter, public:: Min_pc = 0.0, Max_pc = 1100.0
- real, parameter, public:: Min_tc = 160.0, Max_tc = 320.0, Min_tc_std = 0.0, Max_tc_std = 40.0
+ real, parameter, public:: Min_Pc = 0.0, Max_Pc = 1100.0
+ real, parameter, public:: Min_Tc = 160.0, Max_Tc = 320.0, Min_Tc_Std = 0.0, Max_Tc_Std = 40.0
  real, parameter, public:: Min_Tc_Uncer = 0.0, Max_Tc_Uncer = 100.0
  real, parameter, public:: Min_ec = 0.0, Max_ec = 1.0, Min_ec_std = 0.0, Max_ec_std = 1.0
  real, parameter, public:: Min_Beta = 0.0, Max_Beta = 2.0, Min_Beta_std = 0.0, Max_Beta_std = 2.0
  real, parameter, public:: Min_Tau = -0.2, Max_Tau = 160.0, Min_Tau_std = 0.0, Max_Tau_std = 100.0
  real, parameter, public:: Min_Tau_Acha = -0.2, Max_Tau_Acha = 8.0
+ real, parameter, public:: Min_Acha_Cost = 0.0, Max_Acha_Cost = 18.0
  real, parameter, public:: Min_Reff = 0.0, Max_Reff = 160.0, Min_Reff_std = 0.0, Max_Reff_std = 100.0
+ real, parameter, public:: Min_Hcld = 0.0, Max_Hcld = 4000.0
+ real, parameter, public:: Min_Cdnc = 0.0, Max_Cdnc = 1000.0
  real, parameter, public:: Min_lwp = 0.0, Max_lwp = 2000.0, Min_lwp_std = 0.0, Max_lwp_std = 100.0
  real, parameter, public:: Min_iwp = 0.0, Max_iwp = 2000.0, Min_iwp_std = 0.0, Max_iwp_std = 100.0
  real, parameter, public:: Min_frac = 0.0, Max_frac = 1.0

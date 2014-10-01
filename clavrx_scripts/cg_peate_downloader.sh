912f24841a8e093c51307d5b46bb855fa39e9a9a
awalther <awalther@98759d60-67cc-457f-8aaf-b3b51597e956>
2014-10-01 11:21:44 +0000

    new downloader

--- clavrx_scripts/cg_peate_downloader.sh
+++ clavrx_scripts/cg_peate_downloader.sh
@@ -5,16 +5,70 @@ function usage() {
 cat<< EOF
 
 
-need help? sorry!
+need help? bad luck sorry!
 
 EOF
 
 }
 
+
+
+while :; do
+   case $1 in 
+      -h|-\?|--help) 
+         usage
+         exit
+         ;;
+      --lon)
+         if [ "$2" ]; then
+            LON=$2
+            echo $LON
+            shift 2
+            continue 
+         else
+            echo 'ERROR: Must specify a non-empty "--lon lon" argument.' >&2
+            exit 1
+         fi       
+      ;;
+       --lat)
+         if [ "$2" ]; then
+            LAT=$2 
+            
+            shift 2
+            continue 
+         else
+            echo 'mist specify long'
+            exit 1
+         fi       
+      ;;
+      --region)
+         REG=$2
+         shift 2
+         continue
+      ;;
+      --day)
+         DAY='1'
+         
+      ;;
+      
+      --night)
+         NIGHT='1'
+         
+      ;;
+      
+      
+      *)
+      break
+   esac
+   shift
+done
+
 START=$1
 END=$2
 shift 2
 TYPES=$*
+
+echo $TYPES
 
 OUTPUT=wget
 URL="http://peate.ssec.wisc.edu/flo/api/find?start=$START&end=$END&output=$OUTPUT"
@@ -23,11 +77,37 @@ for ft in $TYPES; do
 	URL="$URL&file_type=$ft"
 done
 
+if [ $LON ];
+   then
+   URL="$URL&loc=$LAT,$LON"
+   if [ $REG ];
+      then
+      URL="$URL&radius=$REG"
+   else
+      URL="$URL&radius=2000"   
+   fi
+fi  
+
+if [ $DAY ];
+   then
+   URL="$URL&tod=D"
+fi 
+
+if [ $NIGHT ];
+   then
+   URL="$URL&tod=N"
+fi 
+
+
+
 echo $URL
 
 SCRIPT=downloader.sh
 
 wget -q -O $SCRIPT ${URL}
+bash $SCRIPT
 
-usage
-exit 1
+echo "sucess"
+exit
+
+

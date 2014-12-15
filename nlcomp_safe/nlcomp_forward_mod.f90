0855d38dec677773f9c4fbea2e9ad17e11f0be64
awalther <awalther@98759d60-67cc-457f-8aaf-b3b51597e956>
2014-12-15 18:01:33 +0000

    With BTD ( not really working). Begore start with hybrid

---
+++ nlcomp_safe/nlcomp_forward_mod.f90
@@ -0,0 +1,328 @@
+! $Id: dcomp_forward_mod.f90 385 2014-06-05 20:54:18Z awalther $
+!  update 
+module nlcomp_forward_mod
+
+   use dcomp_lut_mod , only : &
+       lut_obj , lut_output
+ 
+   
+    use dcomp_science_tools_mod, only: &
+      get_planck_radiance_39um  &
+      , get_rad_refl_factor                
+   
+    use clavrx_planck_mod, only: &
+      planck_tmp2rad &
+      , planck_rad2tmp
+   private
+   public :: thick_cloud_cps
+   
+   
+   integer, parameter, public:: real4 = selected_real_kind(6,37)
+   
+   real , parameter :: PI = 4.* ATAN(1.)
+   real , parameter :: DTOR = PI / 180.
+   
+   type, public :: pixel_vec
+      real :: sol_zen
+	   real :: sat_zen
+	   real :: rel_azi
+      real :: lun_zen
+      real :: lun_rel_azi
+	   real :: ctt
+      logical :: is_water_phase
+   end type pixel_vec
+    
+   public :: nlcomp_forward_computation
+   public :: compute_cld_albedo_vis
+   
+   real , save :: planck_rad20
+   real , save :: rad_to_refl 
+   
+contains
+   
+   subroutine nlcomp_forward_computation ( &
+                                    state_vec  &   ! -input
+                                  , pixel &
+                                  , sensor &
+                                  
+                                  , alb_sfc &
+                                  , air_trans_ac &
+                                  , fm_vec &         ! - output
+                                  , cld_trns_sol &
+                                  , cld_trns_sat &
+                                  , cld_sph_alb &
+                                  , kernel & 
+                                  , rad_abv_cld &
+                                  , rad_clear_toc  &
+                                  , lut_path &
+                                  , cld_albedo_vis  )
+       
+       
+                          
+   
+      implicit none
+      
+      real , intent(in) :: state_vec (2)
+      type ( pixel_vec ) , intent ( in ) :: pixel
+      
+      real,  intent ( in ) :: air_trans_ac ( : )   
+      character ( len = * ) , intent (in ) :: sensor     
+      real, intent(in) ::  alb_sfc (2)  
+      real, optional, intent ( in ) ::   rad_abv_cld (42)
+      real, optional, intent ( in ) ::   rad_clear_toc(42) 
+      character ( len = 1024 ) , intent ( in ) , optional :: lut_path  
+         
+      real , intent ( out ) :: fm_vec ( 4 )
+      real , intent ( out ) :: kernel( 4 , 2 )
+      real , intent ( out ) :: cld_trns_sol( 2 ) 
+      real , intent ( out ) :: cld_trns_sat( 2 )
+      real , intent ( out ) :: cld_sph_alb( 2 )
+      real , intent ( out ) , optional :: cld_albedo_vis
+    
+ 
+      integer :: phase_num
+      
+      real :: fm_nir_terr 
+      real :: kernel_nir_terr_cod
+      real :: kernel_nir_terr_cps         
+      real :: trans_two_way
+      real :: air_mass_two_way
+      real :: air_mass_sat
+      real :: trans_abv_cld
+         
+	   type ( lut_output) :: lut_data , lut_data31, lut_data32 , lut_data20
+
+     
+      
+      integer :: MODIS_EQUIVALENT_39_CHN = 20
+	   real :: Alb_Sfc_Term
+      
+      integer , parameter :: VIS_CHN = 42
+      real :: bt31, bt32, bt20
+      real :: rad31, rad32, rad20
+      real :: planck_rad31, planck_rad32
+      
+      
+      real :: rad20_dcod
+      real :: rad31_dcod
+      real :: rad32_dcod
+
+      real :: rad20_dcps
+      real :: rad31_dcps
+      real :: rad32_dcps
+
+      real :: bt20_dcod
+      real :: bt31_dcod
+      real :: bt32_dcod
+
+      real :: bt20_dcps
+      real :: bt31_dcps
+      real :: bt32_dcps
+      
+    
+	   ! - executable
+      
+      
+      ! initialize lut object
+      call lut_obj % initialize ( 'VIIRS' , ancil_path = lut_path)
+      call lut_obj % set_angles ( pixel % sat_zen , pixel % lun_zen, pixel % lun_rel_azi )
+      
+    
+      
+      phase_num = 2
+      if ( pixel % is_water_phase ) phase_num = 1
+      
+      
+      air_mass_two_way = ( 1. / cos (pixel % lun_zen * DTOR ) + 1./ cos ( pixel % Sat_zen * DTOR ) )
+      
+      
+      planck_rad20 = planck_tmp2rad ( pixel % ctt, trim(sensor), 20)
+      planck_rad31 = planck_tmp2rad ( pixel % ctt, trim(sensor), 31)
+      planck_rad32 = planck_tmp2rad ( pixel % ctt, trim(sensor), 32)
+      
+      
+      ! forward element1
+      
+      call lut_obj % get_data ( 1 , phase_num , state_vec(1), state_vec(2) , lut_data)
+     
+      cld_trns_sol ( 1 ) = lut_data % trn_sol
+      cld_trns_sat ( 1 ) = lut_data % trn_sat
+      cld_sph_alb  ( 1 )  = lut_data % albsph
+      !cld_albedo_vis  =  lut_data % alb
+       
+      Alb_Sfc_Term = max (0. , Alb_Sfc( 1 ) / (1.0 - Alb_Sfc(1 ) * lut_data % albsph )) 
+      fm_vec(1) = lut_data% Refl + Alb_Sfc_Term * lut_data%Trn_sol * lut_data%Trn_sat 
+       
+      kernel(1,1) = lut_data%dRefl_dcod &
+                     & +  Alb_Sfc_Term * lut_data%Trn_sol *  lut_data % dTrans_sat_dcod &
+                     & + Alb_Sfc_Term * lut_data%Trn_sat  * lut_data%Dtrans_sol_Dcod &
+                     & +  ((lut_data%Trn_sol * lut_data%Trn_sat  * Alb_Sfc( 1 ) * Alb_Sfc( 1) &
+                     & * lut_data%Dsph_alb_Dcod) &
+                    /(( 1 - Alb_Sfc( 1 ) * lut_data%albsph)**2))  
+                                             
+         kernel(1,2) = lut_data%dRefl_dcps &
+                     & +  Alb_Sfc_Term * lut_data%Trn_sol *  lut_data % dTrans_sat_dcps &
+                     & + Alb_Sfc_Term * lut_data%Trn_sat  * lut_data%Dtrans_sol_Dcps &
+                     & +  ((lut_data%Trn_sol * lut_data%Trn_sat  * Alb_Sfc( 1 ) * Alb_Sfc( 1 ) &
+                     & * lut_data%Dsph_alb_Dcps) &
+                    /(( 1 - Alb_Sfc( 1) * lut_data%albsph)**2))  
+                    
+      trans_two_way = air_trans_ac( 1 ) ** air_mass_two_way   
+      
+      fm_vec(1) = fm_vec(1) * trans_two_way
+	   kernel(1,1) = kernel(1,1) * trans_two_way
+      kernel(1,2) = kernel(1,2) * trans_two_way
+      
+      ! - forward element2
+     
+      call lut_obj % get_data ( 20, phase_num , state_vec(1), state_vec(2) , lut_data20)
+     
+      
+
+	   air_mass_sat =  1./ cos ( pixel % Sat_zen * dtor) 
+		trans_abv_cld = air_trans_ac(2) ** air_mass_sat
+                                   
+      fm_nir_terr =   (rad_abv_cld(20) * lut_data20 % ems &
+                  + trans_abv_cld * lut_data20 %  ems * planck_rad20 &
+				  + lut_data20 % trn_ems * rad_clear_toc(20) )
+            
+      kernel_nir_terr_cod =  (rad_abv_cld(20) * lut_data20 %  Dems_Dcod & 
+               + trans_abv_cld * Planck_Rad20 * lut_data20 % Dems_Dcod  &
+               + rad_clear_toc(20) *lut_data20 % Dtrnems_Dcod)
+             
+      kernel_nir_terr_cps  =   (rad_abv_cld(20) * lut_data20 % Dems_Dcps & 
+                + trans_abv_cld * Planck_Rad20 * lut_data20 % Dems_Dcps  &
+                + rad_clear_toc(20)  *lut_data20 % Dtrnems_Dcps )
+                       
+      fm_vec(2) = fm_nir_terr
+      kernel ( 2, 1) = kernel_nir_terr_cod 
+      kernel ( 2, 2) = kernel_nir_terr_cps 
+            
+     
+    
+      call lut_obj % get_data ( 31, phase_num , state_vec(1), state_vec(2) , lut_data31)
+      
+      call lut_obj % get_data ( 32, phase_num , state_vec(1), state_vec(2) , lut_data32)  
+      
+      call lut_obj % get_data ( 20, phase_num , state_vec(1), state_vec(2) , lut_data20)
+     
+      rad20 = lut_data20 %  ems * planck_rad20 + lut_data20 % trn_ems * rad_clear_toc(20)            
+      rad31 = lut_data31 %  ems * planck_rad31 + lut_data31 % trn_ems * rad_clear_toc(31)
+      rad32 = lut_data32 %  ems * planck_rad32 + lut_data32 % trn_ems * rad_clear_toc(32) 
+     print*,'rad31: ',lut_data31 %  ems ,planck_rad31, lut_data31 % trn_ems , rad_clear_toc(31)
+      bt20 =  planck_rad2tmp ( rad20, trim(sensor), 20)    
+      bt31 =  planck_rad2tmp ( rad31, trim(sensor), 31)
+      bt32 =  planck_rad2tmp ( rad32, trim(sensor), 32)  
+      
+    
+      rad20_dcod = lut_data20 % Dems_Dcod * planck_rad20 + lut_data20 % Dtrnems_Dcod  * rad_clear_toc(20)
+      rad31_dcod = lut_data31 % Dems_Dcod * planck_rad31 + lut_data31 % Dtrnems_Dcod  * rad_clear_toc(31)
+      rad32_dcod = lut_data32 % Dems_Dcod * planck_rad32 + lut_data32 % Dtrnems_Dcod  * rad_clear_toc(32)
+     
+      rad20_dcps = lut_data20 % Dems_Dcps * planck_rad20 + lut_data20 % Dtrnems_Dcps  * rad_clear_toc(20)
+      rad31_dcps = lut_data31 % Dems_Dcps * planck_rad31 + lut_data31 % Dtrnems_Dcps  * rad_clear_toc(31)
+      rad32_dcps = lut_data32 % Dems_Dcps * planck_rad32 + lut_data32 % Dtrnems_Dcps  * rad_clear_toc(32)
+   
+      
+      
+      bt20_dcod =  100.* (planck_rad2tmp ( rad20 + rad20_dcod/100. , trim(sensor), 20) - bt20)
+      bt31_dcod =  100.* (planck_rad2tmp ( rad31 + rad31_dcod/100., trim(sensor), 31)  - bt31)
+      bt32_dcod =  100.* (planck_rad2tmp ( rad32 + rad32_dcod/100., trim(sensor), 32) - bt32)
+     
+      
+      bt20_dcps =  100.*(planck_rad2tmp ( rad20 + rad20_dcps/100., trim(sensor), 20)- bt20)
+      bt31_dcps =  100.* (planck_rad2tmp (  rad31 + rad31_dcps/100., trim(sensor), 31)-bt31)
+      bt32_dcps =  100.* (planck_rad2tmp ( rad32 + rad32_dcps/100., trim(sensor), 32) - bt32)
+      
+   
+   
+      fm_vec(3) = bt31 - bt32
+      print*,rad31,rad32,rad20
+      print*,bt31,bt32,bt20
+      
+            
+      kernel ( 3,1) = bt31_dcod - bt32_dcod
+      kernel ( 3,2) = bt31_dcps - bt32_dcps                    
+                          
+     
+      ! element 4
+  
+   
+      fm_vec(4) = bt20 - bt31
+     
+     
+      
+      kernel ( 4,1) =   bt20_dcod - bt31_dcod                     
+      kernel ( 4,2) =  bt20_dcps - bt31_dcps
+      
+      
+        
+   end subroutine nlcomp_forward_computation
+    
+   !------------------------------------------------------------------------------
+   !
+   !------------------------------------------------------------------------------
+   function thick_cloud_cps ( rfl_nir_obs,  pixel  ) result ( cps)
+      implicit none
+   
+      real,  intent(in) :: rfl_nir_obs
+      type ( pixel_vec) , intent(in) :: pixel
+      
+      
+      
+      
+      integer :: channel_nir = 20
+      real :: cps
+      
+      ! -- local
+      
+      integer  :: cps_pos          ! position in LUT
+      real     :: cps_wgt ! position weights in LUT
+      integer :: i
+      real :: cps_vec_lut (9)   !- CPS vector in LUT      
+      real :: rad (9) 
+      real :: rfl_terr (9) 
+     
+      real :: Ems_lut(9)  ! - sub table
+      real :: rfl_lut(9)
+      real :: Rfl (9)						! - Reflectance at TOA under consideration of surface impact
+      integer :: ii
+      integer :: phase_num
+      
+      ! - executable
+      
+      ! - cps lut range [0.4,0.6,...,2.0]
+      cps_vec_lut = (/(i,i=4,20,2)/)/10.
+      
+      phase_num = 2
+      if ( pixel % is_water_phase ) phase_num = 1
+      
+      call lut_obj % thick_cloud_rfl ( channel_nir  , phase_num  , rfl_lut, ems_lut )
+   
+      rfl = rfl_lut  
+           
+         rad =  ems_lut * Planck_Rad20 
+         rfl_terr = rad * Rad_To_Refl
+         rfl = rfl +  rfl_terr
+     
+      
+      cps_pos = -999
+      cps = -999.
+ 
+      do ii = 1, 8
+         if ( rfl(ii)-rfl_nir_obs > 0 .and. rfl(ii+1)-rfl_nir_obs < 0 ) cps_pos = ii
+      end do
+
+      if (cps_pos .NE. -999) then
+         cps_wgt = (rfl(cps_pos) - rfl_nir_obs)/(rfl(cps_pos) - rfl(cps_pos+1))
+         cps = CPS_Vec_LUT(cps_pos) + (cps_wgt * 0.2)
+      end if
+ 
+      
+      return					 
+      
+      
+   end function thick_cloud_cps
+ 
+       
+end module nlcomp_forward_mod
---
+++ nlcomp_safe/nlcomp_one_pixel_run_program.f90
@@ -0,0 +1,179 @@
+! $Header: https://svn.ssec.wisc.edu/repos/cloud_team_nlcomp/trunk/nlcomp.f90 8 2014-01-31 08:14:58Z awalther $
+program nlcomp_one_pixel_run
+
+   use M_kracken
+   
+   use nlcomp_retrieval_mod, only: &
+      nlcomp_algorithm	&
+      ,  nlcomp_output_type &
+      ,  nlcomp_input_type
+   
+   use  nlcomp_interface_def_mod  , only:  
+		   
+   implicit none
+   real , dimension (20) :: obs, obs_u , alb_sfc , alb_sfc_u , air_trans_ac
+   real, dimension(2) ::  state_apr
+   real :: rad_abv_cld , rad_sfc
+    character( len = 2) :: color_string
+   integer :: start_day
+   real :: sol_zen , sat_zen , rel_azi, cld_temp , lun_zen
+   logical :: snow
+   character (len = 100)  :: text
+   real :: cod , cps , codu , cpsu
+   character(len=20) :: sensor
+  type ( nlcomp_output_type ) :: nlcomp_results
+  type ( nlcomp_input_type) :: inp_retr
+   
+   logical :: water_phase
+   
+   integer :: debug_mode
+   character ( len =20) :: host
+   
+   character ( len = 1024 ) :: ancil_path
+
+!  dummy input
+! make cool input later
+  
+  
+   call getenv("HOST",host)
+   start_day = 30
+    
+   call kracken ("cmd" , "-obs1 0.5211 -obs2 0.133 -alb1 0.12 -alb2 0.12 &        
+                & -ctt 276. -sol 123.33 -sat 21. -azi 80 -lun 53 &
+					 & -apr1 1.0 -apr2 1.0  -snow ""#N#"" -wat ""#N#""  &
+					 & -radabv 0.0002 -radsfc 0.14 -tvis 0.8 -dbg 0 &
+					 & -obsu1 0.01 -obsu2 0.01 -albu1 0.01 -albu2 0.01  -tnr 0.8  -qq 0.8  ")
+					 
+    
+	
+   obs(1) = rget ("cmd_obs1")
+   obs(2) = rget ("cmd_obs2")
+   alb_sfc(1) = rget ("cmd_alb1")
+   alb_sfc(2) = rget ("cmd_alb2")
+   cld_temp = rget ("cmd_ctt")
+   sol_zen  = rget ("cmd_sol")
+   lun_zen = rget ("cmd_lun")
+   sat_zen = rget ("cmd_sat")
+   rel_azi = rget ("cmd_azi")
+   rad_abv_cld = rget("cmd_radabv")
+   
+   rad_sfc = rget("cmd_radsfc")
+   
+   alb_sfc_u(1) = rget("cmd_albu1")
+   alb_sfc_u(2) = rget("cmd_albu2")
+   obs_u(1) = rget("cmd_obsu1")
+   obs_u(2) = rget("cmd_obsu2")
+   air_trans_ac(1) = rget("cmd_tvis")
+   air_trans_ac(2) = rget("cmd_tnr")
+ 
+   
+   state_apr(1) = rget ("cmd_apr1")
+   state_apr(2) = rget ("cmd_apr2")
+   snow = lget("cmd_snow")
+  
+   water_phase = lget ("cmd_wat")
+  
+   debug_mode = iget("cmd_dbg")
+   sensor = 'VIIRS' 
+   !sensor = sensor(:iflen)	
+	
+   
+   if ( debug_mode == 3 ) then 
+      print*, 'NLCOMP stand alone processing'
+	  print*,'to switch on this information set -dbg 3'
+	  print*,'show usage set -dbg 2'
+	  print*
+	  print*,'input:   '
+	  print*,'obs: ', obs(1:2)
+	  print*,'sol, sat, rel_azi: ', sol_zen, sat_zen, rel_azi
+	  print*,'alb_sfc: ',alb_sfc(1:2)
+	  print*,'cloud top temperature: ', cld_temp,trim('K')
+	  print*,'snow: ',snow
+	  print*,'water phase: ', water_phase
+     
+      print*,'above cloud transmission: ', air_trans_ac(1:2)
+      print*
+   end if
+   
+   
+   if ( debug_mode == 2 ) then 
+      print*, 'DCOMP stand-alone processing'
+	   print*,'to switch on this information set -dbg 2'
+	   print*
+	   print*,'usage: ...'
+	   print*,' this shows the options with the default values if you do not set them: '
+	   print*
+	   print*,'./nlcomp -obs1 0.5211 -obs2 0.133 -alb1 0.12 -alb2 0.12'    
+      print* ,' -ctt 276. -sol 23.33 -sat 21. -azi 80 '
+	   print*,'-apr1 1.0 -apr2 1.0 -sen GOES-15'
+	   print*,'-radabv 0.0002 -radsfc 0.14 -tvis 0.8 -dbg 0 '
+	   print*,'-obsu1 0.01 -obsu2 0.01 -albu1 0.01 -albu2 0.01  -tnr 0.8  -qq 0.8  '
+      print*,' boolean keywords :  -snow -wat'
+	   print*
+   end if
+   
+   ancil_path = '/data3/Ancil_Data/clavrx_ancil_data/luts/cld/'
+   ancil_path = '/DATA/Ancil_Data/clavrx_ancil_data/luts/cld/' 
+   if ( host(1:4) == 'luna' ) ancil_path = '/DATA/Ancil_Data/clavrx_ancil_data/luts/cld/' 
+   if ( host(1:4) == 'saga' ) ancil_path = '/data/Ancil_Data/clavrx_ancil_data/luts/cld/' 
+   
+   
+   inp_retr % conf % ancil_path = trim(ancil_path)
+   
+   
+
+   inp_retr % conf % debug_in = 4
+     
+   inp_retr % geo % sol_zen = sol_zen   
+   inp_retr % geo % lun_zen = lun_zen   
+   inp_retr % geo % sat_zen = sat_zen
+   inp_retr % geo % rel_azi = rel_azi
+   inp_retr % geo % lun_rel_azi = rel_azi
+
+   inp_retr % prd % ctt = cld_temp  
+   inp_retr % prd % cph = water_phase
+    
+   ! - apriori
+   inp_retr % state % a_priori (1) = state_apr(1)
+  
+   inp_retr % state % a_priori (2) =state_apr(2)
+
+
+
+   inp_retr % chn ( 20 ) % rad = obs(2)
+   inp_retr % chn ( 20 ) % rad_u = obs_u(2)
+   inp_retr % chn ( 20 ) % alb_sfc = alb_sfc ( 2)
+   inp_retr % chn ( 20 ) % alb_sfc_u =alb_sfc_u(2)
+   inp_retr % chn ( 20 ) % trans_air_abvcld = 0.9
+   inp_retr % chn ( 20 ) % rad_abvcld_nwp = rad_abv_cld
+   inp_retr % chn ( 20 ) % rad_sfc_nwp = rad_sfc
+
+   inp_retr % chn ( 31 ) % rad = 33.
+   inp_retr % chn ( 31 ) % rad_u = 0.
+   inp_retr % chn ( 31 ) % alb_sfc = 0.05
+   inp_retr % chn ( 31 ) % alb_sfc_u = 0.05
+   inp_retr % chn ( 31 ) % rad_sfc_nwp = 11
+
+   inp_retr % chn ( 32 ) % rad = 34.8
+   inp_retr % chn ( 32 ) % rad_u = 5.
+   inp_retr % chn ( 32 ) % alb_sfc = 0.05
+   inp_retr % chn ( 32 ) % alb_sfc_u = 0.05
+         
+   inp_retr % chn ( 42 ) % rfl   = obs(1)
+   inp_retr % chn ( 42 ) % rfl_u = obs_u(1)
+   inp_retr % chn ( 42 ) % alb_sfc = alb_sfc ( 1)
+   inp_retr % chn ( 42 ) % alb_sfc_u = 0.05
+   inp_retr % chn ( 42 ) % trans_air_abvcld =0.9
+   
+   
+   call nlcomp_algorithm ( inp_retr &
+							  & , nlcomp_results  &
+						 )   ! - output
+   write(color_string,'(I2)') 43
+   text = '============= NLCOMP RESULTS==============='
+   print*,achar(27)//'['//color_string//'m '//trim(text)//achar(27)//'[0m'
+   print*,' ' , trim(sensor), ' NLCOMP output:  COD:',nlcomp_results%cod ,'REF: ', nlcomp_results%cps
+	
+  
+    
+end program nlcomp_one_pixel_run

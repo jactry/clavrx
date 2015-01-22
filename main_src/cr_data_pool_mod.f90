7e4281b5e76367ec3bc3cfc2a66e194ba1b45e7b
awalther <awalther@98759d60-67cc-457f-8aaf-b3b51597e956>
2015-01-22 18:03:54 +0000

    add tools

---
+++ main_src/cr_data_pool_mod.f90
@@ -0,0 +1,16 @@
+module cr_data_pool_mod
+   
+   use sfc_data_mod, only: sfc_main_type
+   use cx_geo_mod, only:lon_lat_index , geo_type
+   use cx_nwp_mod, only:nwp_main_type
+   use cx_sat_mod,only: sat_main_type
+   
+   type ( sfc_main_type ) :: sfc_obj_g
+   type ( geo_type) :: geo_obj_g
+   type ( nwp_main_type ) :: nwp_obj_g
+   type ( sat_main_type ) :: sat_obj_g
+   !type ( rtm_main_type ) :: rtm_obj_g
+   !type ( sen_main_type ) :: sen_obj_g
+
+
+end module cr_data_pool_mod

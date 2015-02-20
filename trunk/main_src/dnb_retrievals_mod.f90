4ee0bea62bffce45d1b852ff6d83097544db9930
awalther <awalther@98759d60-67cc-457f-8aaf-b3b51597e956>
2015-02-20 21:14:26 +0000

    update dnb lunar reflectance coeffs

--- trunk/main_src/dnb_retrievals_mod.f90
+++ trunk/main_src/dnb_retrievals_mod.f90
@@ -89,13 +89,15 @@ contains
       !       waxp5=1.449e-3,waxp6=2.590e-2,waxp7=1.135
       
       
-      ! USE 12/18/2014
-      !  updated coeffcients sent from Steve on 26 Jan 2015 (AW)
+
 
       ! WAX VIS08
     !  DOUBLE PRECISION :: wanp1=5.119e-12,wanp2=-2.113e-9,wanp3=3.485e-7,wanp4=-2.835e-5,&
       !    wanp5=1.151e-3,wanp6=-2.205e-2,wanp7=1.112
-    
+      
+      
+       ! USE 12/18/2014
+      !  updated coeffcients sent from Steve on 26 Jan 2015 (AW)
        DOUBLE PRECISION :: waxp1=5.2228e-12,waxp2=2.2411e-9,waxp3=3.7994e-7,waxp4=3.1637e-5,&
 
        waxp5=1.308e-3,waxp6=2.333e-2,waxp7=1.1448

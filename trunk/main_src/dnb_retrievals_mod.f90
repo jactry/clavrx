9e9c5b37fd641920ecbb73429f6143fa4e682daa
awalther <awalther@98759d60-67cc-457f-8aaf-b3b51597e956>
2015-05-05 19:25:11 +0000

    update lunar coeffs

--- trunk/main_src/dnb_retrievals_mod.f90
+++ trunk/main_src/dnb_retrievals_mod.f90
@@ -75,6 +75,7 @@ contains
       double precision , parameter :: MEAN_EARTH_SUN_DIST = 149598022.6071
       double precision , parameter :: MEAN_EARTH_MOON_DIST = 384400.0
       real , parameter :: EARTH_RADIUS_KM = 6378.140
+      
       real , parameter:: SRF_INTEG = 0.32560294 ! integral of the DNB sensor response function (micron)
       real , parameter:: ASTRO_DARK_THRESH = 109.0 ! Sun 19 degrees or more below horizon
       real , parameter :: MIN_LUNAR_IRRAD_DNB = 1.0e-5 ! W/m^2 = 1.0e-09 W/cm^2, threshold for doing calcs 
@@ -85,31 +86,22 @@ contains
       ! Note: "lpds" means "lunar phase, degrees, signed"
       DOUBLE PRECISION ::  lpds
  
-      ! DOUBLE PRECISION :: waxp1=6.052e-12,waxp2=2.550e-9,waxp3=4.265e-7,waxp4=3.520e-5,&
-      !       waxp5=1.449e-3,waxp6=2.590e-2,waxp7=1.135
-      
-      
-
+      ! USE 4/10/2015 more precision
 
       ! WAX VIS08
-      !DOUBLE PRECISION :: wanp1=5.119e-12,wanp2=-2.113e-9,wanp3=3.485e-7,wanp4=-2.835e-5,&
-       !   wanp5=1.151e-3,wanp6=-2.205e-2,wanp7=1.112
-      
-      
-       ! USE 12/18/2014
-       ! updated coeffcients sent from Steve on 26 Jan 2015 (AW)
-         DOUBLE PRECISION :: waxp1=5.2228e-12,waxp2=2.2411e-9,waxp3=3.7994e-7,waxp4=3.1637e-5,&
 
-        waxp5=1.308e-3,waxp6=2.333e-2,waxp7=1.1448
+      DOUBLE PRECISION :: waxp1=5.2228373e-12,waxp2=2.2410515e-9,waxp3=3.7994231e-7,waxp4=3.1637454e-5,&
+
+          waxp5=1.3079265e-3,waxp6=2.3328238e-2,waxp7=1.1448359
 
       ! WANE VIS08
 
-      DOUBLE PRECISION :: wanp1=6.3456e-12,wanp2=-2.6096e-9,wanp3=4.2557e-7,wanp4=-3.4088e-5,&
-        wanp5=1.356e-3,wanp6=-2.504e-2,wanp7=1.1451
+      DOUBLE PRECISION :: wanp1=6.3455594e-12,wanp2=-2.6095939e-9,wanp3=4.2557303e-7,wanp4=-3.4087847e-5,&
+         wanp5=1.3562948e-3,wanp6= -2.5037150e-2,wanp7=1.1450824
       
       
          !- other params
-      real , parameter:: PI = 3.1415926536
+      real , parameter:: PI = ACOS(-1.)
       real , parameter::  DTOR = PI / 180.
       integer :: FLOAT_SIZE  = 4
       integer :: DOUBLE_SIZE = 8

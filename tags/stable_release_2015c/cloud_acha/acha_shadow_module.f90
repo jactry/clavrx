448ad7caf75b7e51ba3b32ee9e91b120b09945da
dbotambekov <dbotambekov@98759d60-67cc-457f-8aaf-b3b51597e956>
2015-06-23 19:57:21 +0000

    new stable release from trunk version 1281

---
+++ tags/stable_release_2015c/cloud_acha/acha_shadow_module.f90
@@ -0,0 +1,205 @@
+! $Id$
+!--------------------------------------------------------------------------------------
+! Clouds from AVHRR Extended (CLAVR-x) 1b PROCESSING SOFTWARE Version 5.3
+!
+! NAME: acha_shadow_module.f90 (src)
+!       ACHA_SHADOW (program)
+!
+! PURPOSE: this module computes the geometrical shadow routine
+!
+! DESCRIPTION: 
+!
+! AUTHORS:  Andi Walther, UW/SSEC/CIMSS
+!
+! COPYRIGHT
+! THIS SOFTWARE AND ITS DOCUMENTATION ARE CONSIDERED TO BE IN THE PUBLIC
+! DOMAIN AND THUS ARE AVAILABLE FOR UNRESTRICTED PUBLIC USE. THEY ARE
+! FURNISHED "AS IS." THE AUTHORS, THE UNITED STATES GOVERNMENT, ITS
+! INSTRUMENTALITIES, OFFICERS, EMPLOYEES, AND AGENTS MAKE NO WARRANTY,
+! EXPRESS OR IMPLIED, AS TO THE USEFULNESS OF THE SOFTWARE AND
+! DOCUMENTATION FOR ANY PURPOSE. THEY ASSUME NO RESPONSIBILITY (1) FOR
+! THE USE OF THE SOFTWARE AND DOCUMENTATION; OR (2) TO PROVIDE TECHNICAL
+! SUPPORT TO USERS.
+!
+! Public routines used in this MODULE:
+!
+!--------------------------------------------------------------------------------------
+module ACHA_SHADOW
+
+ implicit none
+
+ public:: CLOUD_SHADOW_RETR
+
+ private:: SHADOW_IND          
+
+ contains
+
+!==============================================================================
+! Cloud Shadow Routine
+!
+! -1 = shadow estimation not possible
+!  0 = no shadow
+!  1 = shadow determined from geometrical analysis
+!  2 = shadow determined from spectral analysis
+!  3 = shadow determined from both geometrical and spectral analysis
+!
+!==============================================================================
+subroutine CLOUD_SHADOW_RETR (  &
+           Cloud_Height &
+         , Solar_Azi &
+         , Solar_Zenith &
+         , Lat &
+         , Lon &
+         , Lat_Pc &
+         , Lon_Pc &
+         , Cloud_Shadow )
+      
+      implicit none 
+      real, intent(in) :: Cloud_Height (:,:)
+      real, intent(in) :: Solar_Azi (:,:)
+      real, intent(in) :: Solar_Zenith (:,:)
+      real, intent(in) :: Lat (:,:)
+      real, intent(in) :: Lon (:,:)  
+      real, intent(in) :: Lat_Pc (:,:)
+      real, intent(in) :: Lon_Pc (:,:)
+      
+      integer(1), intent(out) :: Cloud_Shadow (:,:)
+      
+      real, parameter :: PI = 3.1415926535897
+      real, parameter :: DTOR = PI/180.
+      real, allocatable :: Distance_km (:,:)
+      real, allocatable :: Lon_Spacing_Per_m (:,:)
+      real,parameter:: LAT_SPACING_PER_M = 8.9932e-06   ! ( = 1.0/111000.0 m )
+      
+      integer :: i,j
+      real :: delta_lon, delta_Lat
+      integer :: i_dim, j_dim
+      
+      !--- initialize output
+      cloud_shadow = 0
+
+      !--- allocate local memory
+      i_dim = size ( Cloud_Height, dim=1)
+      j_dim = size ( Cloud_Height, dim=2)
+      allocate ( Distance_km (i_dim, j_dim))
+      allocate ( Lon_Spacing_Per_m (i_dim, j_dim))
+
+      Distance_km = Cloud_Height * tan (Solar_Zenith * DTOR )
+      
+      Lon_Spacing_Per_m = LAT_SPACING_PER_M / cos ( Lat_Pc * DTOR )
+      
+         do i = 2 , i_dim - 1
+            do j = 2 ,j_dim - 1
+               if ( Solar_Zenith (i,j) > 85.0 ) cycle
+               if ( Cloud_Height (i,j) <= 0.0 ) cycle
+               
+               ! are there clear pixels around at all?
+               if ( count ( Cloud_Height (i-1:i+1,j-1:j+1) > 0.0 ) == 9 ) cycle
+
+               Delta_Lon = -1.0 * sin(Solar_Azi(i,j) * DTOR ) * Distance_km(i,j) * Lon_Spacing_Per_m(i,j)
+               Delta_Lat = -1.0 * cos(Solar_Azi(i,j) * DTOR ) * Distance_km(i,j) * Lat_Spacing_Per_m
+                
+               call SHADOW_IND ( Lat_Pc(i,j) + Delta_Lat, Lon_Pc(i,j) + Delta_Lon, Lat, Lon, i, j, Cloud_Shadow) 
+              
+            end do
+         end do   
+         
+         !--- destroy local memory
+         deallocate ( Distance_km)
+         deallocate ( Lon_Spacing_Per_m)
+
+   end subroutine CLOUD_SHADOW_RETR
+   !================================================================================================================ 
+   !  
+   !  input:
+   !     Lat1,lon1 : the lon/Lat values of maximal shadow
+   !
+   !     Lat , Lon : lon/Lat coordinate array
+   !    
+   !      i , j : the cloud pixel for Lat/Lon array
+   !
+   !  output : 
+   !      shad_arr: one byte integer array with same size as Lat, lon (0 = false, 1 = true)
+   !
+   !================================================================================================================ 
+   subroutine SHADOW_IND( Lat1,lon1, Lat , Lon , i, j, shad_arr )
+      real, intent(in) :: Lat1
+      real, intent(in) :: lon1
+      real, intent(in) :: Lat(:,:)
+      real, intent(in) :: Lon(:,:)
+      
+      integer(1) , intent(inout) ::shad_arr (:,:)
+      integer , intent(in) :: i
+      integer , intent(in) :: j
+      
+      real :: pixel_size_Lat (2)
+      real :: pixel_size_Lon (2)
+      integer :: ii,jj
+      real :: diff_Lat , diff_lon
+      real :: delta_Lat_ii , delta_Lat_jj
+      real :: delta_lon_ii , delta_lon_jj
+      integer :: long_idx,short_idx
+      integer :: short_idx_arr
+      
+      integer :: dim_1 , dim_2
+      integer :: idx_1 , idx_2
+      integer :: k
+      
+      dim_1 = size ( shad_arr, 1 )
+      dim_2 = size ( shad_arr, 2 )
+      
+      delta_Lat_ii = Lat(i,j) - Lat(i-1,j)
+      delta_lon_ii = Lon(i,j) - Lon(i-1,j)
+      delta_Lat_jj = Lat(i,j) - Lat(i,j-1)
+      delta_lon_jj = Lon(i,j) - Lon(i,j-1)
+     
+      diff_Lat = Lat1 - Lat(i,j)
+      diff_Lon = lon1 - Lon(i,j) 
+      
+      !     use of these equations:
+      ! diff_Lon = ii * delta_lon_ii + jj * delta_lon_jj
+      ! diff_Lat = ii * delta_Lat_ii + jj * delta_Lat_jj
+      !   solve for ii => 
+      
+      ii = ( diff_Lat * delta_lon_jj - diff_Lon * delta_Lat_jj) / &
+         (delta_Lat_ii * delta_lon_jj - delta_lon_ii * delta_Lat_jj)
+      
+      jj = ( diff_Lat - ii * delta_Lat_ii) / delta_Lat_jj
+     
+      ! - find longer dim
+      !
+      long_idx   = maxval (ABS([ii,jj]))
+      short_idx  = minval (ABS([ii,jj]))
+      
+      ! - this fills all pixel from Lat1 to Lat(i,j)/Lon(i,j)  
+      do k = 1 , long_idx        
+         if (abs(ii) == long_idx ) then
+            idx_1 = i + sign(k,ii)
+            if (idx_1 < 1 .or. idx_1 > dim_1 ) cycle
+             
+            short_idx_arr = CEILING ( short_idx * sign(k,jj) / (1.* long_idx ) ) 
+       
+            idx_2 = j + short_idx_arr
+            if ( idx_2 > 0 .and. idx_2 <= dim_2 ) shad_arr( idx_1 , idx_2 )   = 1
+            idx_2 = j + short_idx_arr - 1
+            if ( idx_2 > 0 .and. idx_2 <= dim_2 ) shad_arr( idx_1 , j + short_idx_arr-1) = 1
+         else 
+            idx_2 = j + sign(k,jj)
+          
+            if (idx_2 < 1 .or. idx_2 > dim_2 ) cycle
+            short_idx_arr = CEILING ( short_idx * sign(k,ii) / ( 1.* long_idx)  ) 
+            
+            idx_1 = i + short_idx_arr
+            
+            if ( idx_1 > 0 .and. idx_1 <= dim_1 ) shad_arr(i+short_idx_arr,idx_2 ) = 1
+            idx_1 = i + short_idx_arr - 1
+            if ( idx_1 > 0 .and. idx_1 <= dim_1 ) shad_arr(i+short_idx_arr-1, idx_2) = 1
+         endif      
+      end do
+   
+   end subroutine SHADOW_IND
+
+!-----------------------------------------------------------
+! end of module
+!-----------------------------------------------------------
+end module ACHA_SHADOW
---
+++ tags/stable_release_2015c/cloud_team_cmask/nb_cloud_mask_clavrx_services_module.f90
@@ -0,0 +1,172 @@
+ !-----------------------------------------------------------------------------
+ ! Input Structure
+ !-----------------------------------------------------------------------------
+ module NB_CLOUD_MASK_SERVICES
+
+ implicit none
+
+ include 'nb_cloud_mask.inc'
+
+ integer, parameter, public:: int1 = selected_int_kind(1)
+ integer, parameter, public:: int2 = selected_int_kind(3)
+ integer, parameter, public:: int4 = selected_int_kind(8)
+ integer, parameter, public:: int8 = selected_int_kind(10)
+ integer, parameter, public:: real4 = selected_real_kind(6,37)
+ integer, parameter, public:: real8 = selected_real_kind(15,307)
+
+ !--- missing value
+ real, parameter, public :: MISSING_VALUE_REAL4 = -999.0
+ integer(kind=int1), parameter, public :: MISSING_VALUE_INT1 = -128
+
+ !--- numerical constants
+ real, parameter, public:: pi = 3.14159265
+ real, parameter, public:: dtor = pi/180.0
+
+ type, public :: et_cloudiness_class_type
+      integer :: SPACE 
+      integer :: MISSING
+      integer :: CLOUDY
+      integer :: PROB_CLOUDY
+      integer :: PROB_CLEAR
+      integer :: CLEAR
+ end type
+
+ type, public :: mask_input
+    integer:: Num_Elem                                                 !x-dimension of data arrays
+    integer:: Num_Line                                                 !number of lines of arrays with data
+    integer:: Num_Line_Max                                             !y-dimension of data arrays
+    integer(kind=int1), pointer:: Invalid_Data_Mask                    !bad data mask (0=good,1=bad)
+    integer:: Chan_On_041um                                            !flag if 0.41um channel on (0=no,1=yes)
+    integer:: Chan_On_063um                                            !flag if 0.63um channel on (0=no,1=yes)
+    integer:: Chan_On_086um                                            !flag if 0.86um channel on (0=no,1=yes)
+    integer:: Chan_On_138um                                            !flag if 1.38um channel on (0=no,1=yes)
+    integer:: Chan_On_160um                                            !flag if 1.60um channel on (0=no,1=yes)
+    integer:: Chan_On_213um                                            !flag if 2.13um channel on (0=no,1=yes)
+    integer:: Chan_On_375um                                            !flag if 3.75um channel on (0=no,1=yes)
+    integer:: Chan_On_67um                                             !flag if 6.7um channel on (0=no,1=yes)
+    integer:: Chan_On_85um                                             !flag if 8.5um channel on (0=no,1=yes)
+    integer:: Chan_On_11um                                             !flag if 11.0um channel on (0=no,1=yes)
+    integer:: Chan_On_12um                                             !flag if 12.0um channel on (0=no,1=yes)
+    integer:: Chan_On_I1_064um                                         !flag if I1 0.64um channel on (0=no,1=yes)
+    integer:: Chan_On_I4_374um                                         !flag if I4 3.74um channel on (0=no,1=yes)
+    integer:: Chan_On_I5_114um                                         !flag if I5 11.4um channel on (0=no,1=yes)
+    integer:: Chan_On_DNB                                              !flag if DNB channel on (0=no,1=yes)
+    integer(kind=int1), pointer:: Snow_Class                           !Snow Classification 
+    integer(kind=int1), pointer:: Land_Class                           !Land Classification
+    integer(kind=int1), pointer:: Oceanic_Glint_Mask                   !Mask of oceanic solar glint (0=no,1=yes)
+    integer(kind=int1), pointer:: Lunar_Oceanic_Glint_Mask             !Mask of oceanic lunar glint (0=no,1=yes)
+    integer(kind=int1), pointer:: Coastal_Mask                         !binary coast mask (0=no,1=yes)
+    real(kind=real4), pointer:: Solzen                                 !Solar zenith angle (degrees)
+    real(kind=real4), pointer:: Scatzen                                !Solar Scattering angle (degrees)
+    real(kind=real4), pointer:: Lunscatzen                             !Lunar Scattering angle (degrees)
+    real(kind=real4), pointer:: Senzen                                 !Sensor viewing zenith angle (degrees)
+    real(kind=real4), pointer:: Lunzen                                 !Lunar viewing zenith angle (degrees)
+    real(kind=real4), pointer:: Lat                                    !Latitude (degrees)
+    real(kind=real4), pointer:: Lon                                    !Longitude (degrees)
+    real(kind=real4), pointer:: Ref_041um                              !0.41 um toa reflectance (%)
+    real(kind=real4), pointer:: Ref_063um                              !0.63 um toa reflectance (%)
+    real(kind=real4), pointer:: Ref_063um_Clear                        !0.63 um toa reflectance for clear-sky (%)
+    real(kind=real4), pointer:: Ref_063um_Std                          !0.63 um toa reflectance 3x3 Std.  Dev. (%)
+    real(kind=real4), pointer:: Ref_063um_Min                          !Min 0.63 um toa reflectance over 3x3 (%)
+    real(kind=real4), pointer:: Ref_086um                              !0.86 um toa reflectance (%)
+    real(kind=real4), pointer:: Ref_138um                              !1.38 um toa reflectance (%)
+    real(kind=real4), pointer:: Ref_160um                              !1.60 um toa reflectance (%)
+    real(kind=real4), pointer:: Ref_160um_Clear                        !1.60 um toa reflectance for clear-sky (%)
+    real(kind=real4), pointer:: Ref_375um                              !3.75 um toa reflectance (%)
+    real(kind=real4), pointer:: Ref_375um_Clear                        !3.75 um toa reflectance for clear-sky (%)
+    real(kind=real4), pointer:: Ref_213um                              !2.13 um toa reflectance (%)
+    real(kind=real4), pointer:: Bt_375um                               !3.75 um toa brightness temp (K)
+    real(kind=real4), pointer:: Bt_375um_Std                           !3.75 um toa brightness temp 3x3 Std. Dev. (K)
+    real(kind=real4), pointer:: Emiss_375um                            !3.75 um pseudo toa emissivity
+    real(kind=real4), pointer:: Emiss_375um_Clear                      !3.75 um pseudo toa emissivity clear-sky
+    real(kind=real4), pointer:: Bt_67um                                !6.7 um toa brightness temperature (K)
+    real(kind=real4), pointer:: Bt_85um                                !8.5 um toa brightness temperature (K)
+    real(kind=real4), pointer:: Bt_11um                                !11 um toa brightness temperature (K)
+    real(kind=real4), pointer:: Bt_11um_Std                            !11 um toa brightness temp 3x3 Std Dev (K)
+    real(kind=real4), pointer:: Bt_11um_Max                            !11 um toa brightness temp 3x3 Max (K)
+    real(kind=real4), pointer:: Bt_11um_Clear                          !11 um toa brightness temperature (K)
+    real(kind=real4), pointer:: Emiss_11um_Tropo                       !11 um tropo emiss
+    real(kind=real4), pointer:: Bt_12um                                !12 um toa brightness temperature (K)
+    real(kind=real4), pointer:: Bt_12um_Clear                          !12 um toa bright temp clear-sky (K)
+    real(kind=real4), pointer:: Ref_I1_064um_Std                       !2x2 std I1 064 um reflectance (%)
+    real(kind=real4), pointer:: Bt_I4_374um_Std                        !2x2 std I4 374 um brightness temp (K)
+    real(kind=real4), pointer:: Bt_I5_114um_Std                        !2x2 std I5 114 um brightness temp (K)
+    real(kind=real4), pointer:: Bt_11um_Bt_67um_Covar                  !covariance of 11 and 6.7 um bright temp.
+    real(kind=real4), pointer:: Sst_Anal_Uni                           !3x3 std of background sst field (K)
+    real(kind=real4), pointer:: Emiss_Sfc_375um                        !the surface emissivity at 3.75 um
+    real(kind=real4), pointer:: Rad_Lunar                              !Lunar toa radiance from DNB
+    real(kind=real4), pointer:: Ref_Lunar                              !Lunar reflectance from DNB (%)
+    real(kind=real4), pointer:: Ref_Lunar_Min                          !Min lunar reflectance over 3x3 (%)
+    real(kind=real4), pointer:: Ref_Lunar_Std                          !3x3 std dev of lunar ref from DNB (%)
+    real(kind=real4), pointer:: Ref_Lunar_Clear                        !Lunar reflectance for clear-skies (%)
+    real(kind=real4), pointer:: Zsfc                                   !surface altitude (km)
+    integer:: Num_Segments                                             !number of segments in this data 
+    integer(kind=int1), pointer:: Solar_Contamination_Mask             !binary mask of solar contamination (0=no,1=yes)
+    integer(kind=int1), pointer:: Sfc_Type                             !surface type based on UMD classification
+ end type mask_input 
+
+ !-----------------------------------------------------------------------------
+ ! Output Structure
+ !-----------------------------------------------------------------------------
+ type, public :: mask_output
+    integer(kind=int1), dimension(:), pointer:: Cld_Flags_Packed         !array of packed results 
+    integer(kind=int1), pointer:: Cld_Mask_Bayes             !Derived 4-level cloud mask
+    integer:: Cloud_Mask_Bayesian_Flag                                       !flag to tell if code should run
+    real(kind=real4), pointer:: Posterior_Cld_Probability    !posterior cloud probability (0-1)
+    integer(kind=int1), pointer:: Dust_Mask
+    integer(kind=int1), pointer:: Smoke_Mask
+    integer(kind=int1), pointer:: Fire_Mask
+ end type mask_output
+
+ !-----------------------------------------------------------------------------
+ ! Diagnostic Output Structure
+ !-----------------------------------------------------------------------------
+ type, public :: diag_output
+    real(kind=real4), pointer:: Array_1    !first diagnostic array
+    real(kind=real4), pointer:: Array_2    !first diagnostic array
+    real(kind=real4), pointer:: Array_3    !first diagnostic array
+ end type diag_output 
+ 
+ !-----------------------------------------------------------------------------
+ ! Symbol Structure
+ !-----------------------------------------------------------------------------
+ type, public :: symbol_naive_bayesian
+    integer(kind=int1) :: CLOUDY
+    integer(kind=int1) :: PROB_CLOUDY
+    integer(kind=int1) :: PROB_CLEAR
+    integer(kind=int1) :: CLEAR
+
+    integer(kind=int1) :: NO
+    integer(kind=int1) :: YES
+
+    integer(kind=int1) :: WATER_SFC
+    integer(kind=int1) :: EVERGREEN_NEEDLE_SFC
+    integer(kind=int1) :: EVERGREEN_BROAD_SFC
+    integer(kind=int1) :: DECIDUOUS_NEEDLE_SFC
+    integer(kind=int1) :: DECIDUOUS_BROAD_SFC
+    integer(kind=int1) :: MIXED_FORESTS_SFC
+    integer(kind=int1) :: WOODLANDS_SFC
+    integer(kind=int1) :: WOODED_GRASS_SFC
+    integer(kind=int1) :: CLOSED_SHRUBS_SFC
+    integer(kind=int1) :: OPEN_SHRUBS_SFC
+    integer(kind=int1) :: GRASSES_SFC
+    integer(kind=int1) :: CROPLANDS_SFC
+    integer(kind=int1) :: BARE_SFC
+    integer(kind=int1) :: URBAN_SFC
+
+    integer(kind=int1) :: SHALLOW_OCEAN
+    integer(kind=int1) :: LAND
+    integer(kind=int1) :: COASTLINE
+    integer(kind=int1) :: SHALLOW_INLAND_WATER
+    integer(kind=int1) :: EPHEMERAL_WATER
+    integer(kind=int1) :: DEEP_INLAND_WATER
+    integer(kind=int1) :: MODERATE_OCEAN
+    integer(kind=int1) :: DEEP_OCEAN
+
+    integer(kind=int1) :: NO_SNOW
+    integer(kind=int1) :: SEA_ICE
+    integer(kind=int1) :: SNOW
+ end type symbol_naive_bayesian
+
+
+end module NB_CLOUD_MASK_SERVICES

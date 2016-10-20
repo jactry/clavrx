72323ed59464bcdee231665826f65951afb9f25d
dbotambekov <dbotambekov@98759d60-67cc-457f-8aaf-b3b51597e956>
2016-10-20 18:43:41 +0000

    forward scattering testing

---
+++ branches/forward_scattering/cloud_type/cloud_type_bridge_module.f90
@@ -0,0 +1,431 @@
+! $Header: https://svn.ssec.wisc.edu/repos/cloud_team_clavrx/trunk/main_src/cloud_type_bridge_module.f90 412 2014-06-11 17:35:49Z heidinger $
+!
+! Clouds from AVHRR Extended (CLAVR-x) 1b PROCESSING SOFTWARE Version 
+!
+! NAME: cloud_type_bridge_module.f90 (src)
+!       cloud_type_bridge (program)
+!
+! PURPOSE: 
+!       Builds the interface to Cloud Type algorithm
+!
+! DESCRIPTION: 
+!
+! AUTHORS:
+!  Andrew Heidinger, Andrew.Heidinger@noaa.gov
+!  Andi Walther, CIMSS, andi.walther@ssec.wisc.edu
+!  Denis Botambekov, CIMSS, denis.botambekov@ssec.wisc.edu
+!  William Straka, CIMSS, wstraka@ssec.wisc.edu
+!
+! COPYRIGHT
+! THIS SOFTWARE AND ITS DOCUMENTATION ARE CONSIDERED TO BE IN THE PUBLIC
+! DOMAIN AND THUS ARE AVAILABLE FOR UNRESTRICTED PUBLIC USE. THEY ARE
+! FURNISHED "AS IS." THE AUTHORS, THE UNITED STATES GOVERNMENT, ITS
+! INSTRUMENTALITIES, OFFICERS, EMPLOYEES, AND AGENTS MAKE NO WARRANTY,
+! EXPRESS OR IMPLIED, AS TO THE USEFULNESS OF THE SOFTWARE AND
+! DOCUMENTATION FOR ANY PURPOSE. THEY ASSUME NO RESPONSIBILITY (1) FOR
+! THE USE OF THE SOFTWARE AND DOCUMENTATION; OR (2) TO PROVIDE TECHNICAL
+! SUPPORT TO USERS.
+!
+!  HISTORY:
+!      2014/04/30:    new interface (AW)
+!          /05/02:    add cld_phase (AW)
+!
+!  GLOBAL VARIABLES:
+!
+!    FROM PIXEL_COMMON:
+!      1. work as input
+!        1.1 configuration
+!             Chan_On_Flag_Default                       integer (42)
+!             Image%Number_Of_Elements                                    integer
+!             Image%Number_Of_Lines_Read_This_Segment                             integer
+!        1.2 geo data:                   
+!              solzen                                    real (:,:)
+!              satzen                                    real (:,:)
+!        1.3 surface 
+!        1.4 rtm / statistics 
+!
+!              bt_ch31_max_3x3                           real (:,:)
+!              bt_Ch31_Std_3x3                           real (:,:)
+!              Covar_Ch27_Ch31_5x5                       real (:,:)
+!              Beta_11um_12um_Tropo_Rtm                  real (:,:)
+!              Beta_11um_133um_Tropo_Rtm                  real (:,:)
+!              zen_idx_rtm                               integer (:,:)
+!              i_nwp
+!              j_nwp
+!              i_lrc
+!              j_lrc
+!        1.5 observations
+!              ch                                        type ( observations )  
+!        1.6 products
+!             cld_mask    
+!       
+!      2. work as output  
+!              cld_type                                  integer (:,:) 
+!           
+!     FROM RTM_COMMON:
+!            rtm                                         type ( ) 
+!            p_std_rtm                                   real(:)
+!
+!     CLOUD_TYPE_ALGO_MODULE
+!             cloud_type_pixel                     subroutine
+!                  subroutine which retrieves cloud type
+!             cloud_type_input_type                      type definition
+!                   structure definition of cloud_type retrieval input  
+!
+!     NAIVE_BAYESIAN_CLOUD_MASK_MODULE
+!              et_cloudiness_class                type with enumerators for cloud mask
+!
+!--------------------------------------------------------------------------------------
+
+module cloud_type_bridge_module
+
+   
+   use PIXEL_COMMON, only : &
+        image &
+      , sensor &
+      , geo &
+      , zen_idx_rtm &
+      , ch &
+      , i_nwp &
+      , j_nwp &
+      , Bt_Ch27_Max_3x3 &
+      , Bt_Ch31_Max_3x3 &
+      , Bt_Ch31_Std_3x3 &
+      , Covar_Ch27_Ch31_5x5 &
+      , cld_type &
+      , cld_phase &
+      , cld_mask &
+      , Dust_Mask &
+      , Smoke_Mask &
+      , Fire_Mask &
+      , i_lrc, j_lrc &
+      , Beta_11um_12um_Tropo_Rtm &
+      , Beta_11um_133um_Tropo_Rtm &
+      , Diag_Pix_Array_1 &
+      , Diag_Pix_Array_2 &
+      , Diag_Pix_Array_3 &
+      , bad_pixel_mask
+                 
+   use CONSTANTS, only : &
+        Cloud_Type_Version
+
+   use CLOUD_TYPE_ALGO_MODULE, only : &
+       cloud_type_pixel &
+       , cloud_type_input_type &
+       , cloud_type_diag_type &
+       , et_cloud_type &
+       , set_cloud_phase
+       
+   use NB_CLOUD_MASK, only: &
+      et_cloudiness_class
+   
+   use RTM_COMMON , only: &
+      p_std_rtm &
+      , rtm
+
+   use CLAVRX_MESSAGE_MODULE, only: MESG
+   
+   implicit none
+   
+   public :: CLOUD_TYPE_BRIDGE  
+   public :: Set_CLOUD_TYPE_VERSION
+
+contains
+
+   !====================================================================
+   !  record svn version as a global variable for output to hdf
+   !====================================================================
+   subroutine Set_CLOUD_TYPE_VERSION()
+      Cloud_Type_Version = "$Id: cloud_type_bridge_module.f90 412 2014-06-11 17:35:49Z heidinger $"
+   end subroutine Set_CLOUD_TYPE_VERSION
+
+
+   !====================================================================
+   ! universal cloud type bridge
+   !====================================================================
+   subroutine cloud_type_bridge
+      implicit none
+      
+      type ( cloud_type_input_type) :: type_inp
+      type ( cloud_type_diag_type) :: diag_out
+      
+      integer :: ctype
+      integer :: i , j   
+      integer :: ii , jj
+      real :: ice_prob 
+      integer :: cld_type_lrc
+      logical, save:: First_Call = .true.
+      
+      
+      ! ------  Executable  ------------------------------------
+      if (First_Call .eqv. .true.) then
+        call MESG('Cloud Type starts ', color = 46)
+      endif
+
+      ice_prob = -999.0
+      
+      type_inp % sat % chan_on = Sensor%Chan_On_Flag_Default == 1
+      
+      !-----------    loop over LRC core pixels to get ice probabbilty -----         
+      elem_loop: do  j = 1,Image%Number_Of_Lines_Read_This_Segment
+         line_loop: do i = 1, Image%Number_Of_Elements  
+            
+            if ( bad_pixel_mask (i,j) == 1 ) then
+               cld_type (i,j ) = et_cloud_type % MISSING
+               cycle
+            end if 
+            
+            if (cld_mask (i,j) == et_cloudiness_class % CLEAR ) then
+               cld_type (i,j ) = et_cloud_type % CLEAR
+                cycle
+            end if
+                
+            if (cld_mask (i,j) == et_cloudiness_class % PROB_CLEAR ) then
+               cld_type (i,j ) = et_cloud_type % PROB_CLEAR
+               cycle
+            end if
+
+            if (Dust_Mask (i,j) == 1 ) then
+               cld_type (i,j ) = et_cloud_type % DUST
+               cycle
+            end if
+
+            if (Smoke_Mask (i,j) == 1 ) then
+               cld_type (i,j ) = et_cloud_type % SMOKE
+               cycle
+            end if
+
+            if (Fire_Mask (i,j) == 1 ) then
+               cld_type (i,j ) = et_cloud_type % FIRE
+               cycle
+            end if
+            
+            ! - take only LRC cores
+            if ( i /= i_lrc (i,j) .or. j /= j_lrc (i,j) ) cycle
+                             
+            call POPULATE_INPUT ( i, j , type_inp )
+            call CLOUD_TYPE_PIXEL  ( type_inp, ctype , diag_out, ice_prob_out = ice_prob )
+            cld_type (i,j)  = ctype
+
+            call DEALLOCATE_INP ( type_inp )
+         
+         end do   line_loop
+      end do elem_loop
+
+ 
+      ! - now loop over all non lrc-cores
+      elem_loop1: do  j = 1,Image%Number_Of_Lines_Read_This_Segment
+         line_loop1: do i = 1, Image%Number_Of_Elements  
+            
+            if ( bad_pixel_mask (i,j) == 1 ) then
+               cld_type (i,j ) = et_cloud_type % MISSING
+               cycle
+            end if 
+            
+            if (cld_mask ( i,j) == et_cloudiness_class % CLEAR ) then
+               cld_type (i , j ) = et_cloud_type % CLEAR
+                cycle
+            end if
+                
+            if (cld_mask ( i,j) == et_cloudiness_class % PROB_CLEAR ) then
+               cld_type (i , j ) = et_cloud_type % PROB_CLEAR
+               cycle
+            end if   
+
+            if (Dust_Mask (i,j) == 1 ) then
+               cld_type (i,j ) = et_cloud_type % DUST
+               cycle
+            end if
+
+            if (Smoke_Mask (i,j) == 1 ) then
+               cld_type (i,j ) = et_cloud_type % SMOKE
+               cycle
+            end if
+
+            if (Fire_Mask (i,j) == 1 ) then
+               cld_type (i,j ) = et_cloud_type % FIRE
+               cycle
+            end if
+            
+            ii = i_lrc (i,j)
+            jj = j_lrc (i,j)
+                        
+            !- we dont need the lrc cores again
+            if ( i == ii .and. j == jj ) cycle
+
+            call POPULATE_INPUT ( i, j , type_inp )
+            call CLOUD_TYPE_PIXEL  ( type_inp, ctype , diag_out, ice_prob_out = ice_prob )
+            
+            !--- set lrc value
+            cld_type_lrc = et_cloud_type % UNKNOWN
+            if ( ii > 0 .and. jj > 0) then
+               cld_type_lrc = cld_type (ii , jj )
+            endif
+
+            ! - compare this ctype with LRC
+           
+            !  - identical or lrc is not valid => take the current
+            if ( ctype == cld_type_lrc .or. ii < 1 .or. jj < 1 .or. cld_type_lrc == et_cloud_type%UNKNOWN) then
+               cld_type (i,j)  = ctype
+            else
+
+               ! - if LRC core is water phase ==> use lrc
+               if ( cld_type_lrc >= et_cloud_type % FIRST_WATER &
+                  .and. cld_type_lrc <= et_cloud_type % LAST_WATER ) then
+               
+                   ! AKH says not to overwrite WATER,  FOG or SUPERCOOLED
+                   if ( ctype >= et_cloud_type % FIRST_WATER &
+                        .and. ctype <= et_cloud_type % LAST_WATER ) then
+                        
+                      cld_type(i,j) = ctype
+                   else
+                     ! - the original ice pixels should also be check on supercool, fog or water.
+                     call CLOUD_TYPE_PIXEL  ( type_inp, ctype , diag_out, force_water = .true. )
+                     cld_type (i,j)  = ctype
+                   end if
+                  
+               ! - LRC core is ice phase
+               else if ( (ctype  == et_cloud_type % FOG &
+                  .or. ctype == et_cloud_type % WATER) &
+                  .and. ( cld_type_lrc == et_cloud_type % CIRRUS &
+                  .or. cld_type_lrc == et_cloud_type % OVERLAP &
+                  .or. cld_type_lrc == et_cloud_type % OPAQUE_ICE)) then
+                  
+                     cld_type (i , j ) = et_cloud_type % CIRRUS
+               
+               ! - LRC core is ice phase and current is supercooled => switch to ice
+               else if ( ( cld_type_lrc == et_cloud_type % CIRRUS & 
+                         .or. cld_type_lrc == et_cloud_type % OPAQUE_ICE &
+                         .or. cld_type_lrc == et_cloud_type % OVERLAP ) &
+                        .and. ctype ==  et_cloud_type % SUPERCOOLED ) then
+                  
+                  call CLOUD_TYPE_PIXEL  ( type_inp, ctype , diag_out, force_ice = .true. )
+                     cld_type (i,j)  = ctype
+                    
+               ! -- this is mainly cirrus / opaque ice => keep current
+               else 
+                  cld_type (i,j)  = ctype                                                 
+               end if                     
+            end if
+
+            call DEALLOCATE_INP ( type_inp )
+         
+         end do   line_loop1
+      end do elem_loop1      
+      
+      call set_cloud_phase ( cld_type, cld_phase) 
+
+
+      First_Call = .false.
+      
+   end subroutine cloud_type_bridge
+   
+   ! --------- --------------- ---
+   !
+   !
+   ! --------- ---------------
+   subroutine POPULATE_INPUT ( i, j , type_inp)
+      integer, intent(in) :: i
+      integer, intent(in) :: j
+      type ( cloud_type_input_type) :: type_inp
+      
+      integer :: nwp_lon_idx , nwp_lat_idx
+      integer :: vza_idx
+      
+      integer :: n_rtm_prof
+      
+      
+      Nwp_Lon_Idx = I_Nwp( i , j )
+      Nwp_Lat_Idx = J_Nwp( i , j )
+      Vza_Idx = zen_Idx_Rtm( i , j )            
+      
+      !-----------------------------------------------------------------------------------
+      ! - sat
+      !-----------------------------------------------------------------------------------
+      if (sensor % chan_on_flag_default(31) == 1 ) type_inp % sat % rad_ch31 = ch(31) % rad_toa ( i,j )
+      if (sensor % chan_on_flag_default(31) == 1 ) type_inp % sat % bt_ch31 =  ch(31) % bt_toa  ( i,j )
+      if (sensor % chan_on_flag_default(32) == 1 ) type_inp % sat % bt_ch32 =  ch(32) % bt_toa  ( i,j )
+      if (sensor % chan_on_flag_default(6) == 1 ) type_inp % sat % ref_ch6 =  ch(6)  % ref_toa  ( i,j )
+      if (sensor % chan_on_flag_default(20) == 1 ) type_inp % sat % ref_ch20 = ch(20) % ref_toa ( i,j )
+
+      if (sensor % chan_on_flag_default(27) == 1 ) then
+         type_inp % sat % rad_ch27 = ch(27) % rad_toa (i,j)
+         type_inp % sat % bt_ch27 =  ch(27) % bt_toa  (i,j)
+      end if  
+      
+      if (sensor % chan_on_flag_default(29) == 1 ) then
+         type_inp % sat % rad_ch29 = ch(29) % rad_toa (i,j)
+         type_inp % sat % bt_ch29 =  ch(29) % bt_toa  (i,j)
+      end if         
+      
+      !-----------------------------------------------------------------------------------
+      ! - rtm
+      !-----------------------------------------------------------------------------------
+      n_rtm_prof = size ( rtm(Nwp_Lon_Idx,Nwp_Lat_Idx)%T_prof )
+      allocate ( type_inp % rtm % t_prof(n_rtm_prof ) , source = rtm(Nwp_Lon_Idx,Nwp_Lat_Idx)%T_prof )
+      allocate ( type_inp % rtm % z_prof(n_rtm_prof ) , source = rtm(Nwp_Lon_Idx,Nwp_Lat_Idx)%z_prof )
+      type_inp % rtm % tropo_lev = rtm(Nwp_Lon_Idx,Nwp_Lat_Idx)%Tropo_Level
+      type_inp % rtm % sfc_lev = rtm(Nwp_Lon_Idx,Nwp_Lat_Idx)%sfc_Level
+      
+      
+      
+      if (sensor % chan_on_flag_default(6) == 1 )  then
+         type_inp % rtm % ref_ch6_clear       = ch(6)%Ref_Toa_Clear( i,j )
+      endif
+      if (sensor % chan_on_flag_default(31) == 1 )  then
+         
+         
+         allocate ( type_inp % rtm % rad_ch31_bb_prof (n_rtm_prof ) &
+              , source = Rtm(Nwp_Lon_Idx,Nwp_Lat_Idx)%d(Vza_Idx)%ch(31)%Rad_BB_Cloud_Profile)
+          
+         type_inp % rtm % bt_ch31_3x3_max    = Bt_Ch31_Max_3x3( i,j )
+         type_inp % rtm % bt_ch31_3x3_std    = Bt_Ch31_Std_3x3( i,j )
+         type_inp % rtm % rad_ch31_atm_sfc   = ch(31)%Rad_Toa_Clear(i,j)
+         type_inp % rtm % bt_ch31_atm_sfc    = ch(31)%Bt_Toa_Clear( i,j )
+         type_inp % rtm % emiss_tropo_ch31   = ch(31)%Emiss_Tropo( i,j )
+         if (sensor % chan_on_flag_default(27) == 1 )  then
+            type_inp % rtm % Covar_Ch27_Ch31_5x5 = Covar_Ch27_Ch31_5x5( i,j )
+         endif
+         if (sensor % chan_on_flag_default(32) ==1 )  then
+            type_inp % rtm % Beta_11um_12um_Tropo  = Beta_11um_12um_Tropo_Rtm( i,j )
+            type_inp % rtm % bt_ch32_atm_sfc       = ch(32)%Bt_Toa_Clear( i,j )
+         endif
+         if (sensor % chan_on_flag_default(33) ==1 )  then
+            type_inp % rtm % Beta_11um_133um_Tropo = Beta_11um_133um_Tropo_Rtm( i,j )
+         endif
+      endif
+      
+      if (sensor % chan_on_flag_default(27) == 1) then
+         type_inp % rtm % bt_ch27_3x3_max    = Bt_Ch27_Max_3x3( i,j )
+         type_inp % rtm % rad_ch27_atm_sfc = ch(27)%Rad_Toa_Clear(i,j)
+         allocate ( type_inp % rtm % rad_ch27_bb_prof(n_rtm_prof ) &
+         , source = Rtm(Nwp_Lon_Idx,Nwp_Lat_Idx)%d(Vza_Idx)%ch(27)%Rad_BB_Cloud_Profile)
+      end if   
+      
+      !-----------------------------------------------------------------------------------
+      ! - geo
+      !-----------------------------------------------------------------------------------
+      type_inp % geo % sol_zen     = geo % solzen ( i , j )
+      type_inp % geo % sat_zen     = geo % satzen ( i , j )
+      
+      !-----------------------------------------------------------------------------------
+      !- sfc
+      !-----------------------------------------------------------------------------------
+      type_inp % sfc % emiss_ch20     = ch(20) % sfc_emiss ( i , j )
+            
+   end subroutine POPULATE_INPUT
+   
+   !----------------------------------------------------------------------------------------- 
+   !
+   !----------------------------------------------------------------------------------------- 
+   subroutine DEALLOCATE_INP ( type_inp)
+      type ( cloud_type_input_type) :: type_inp
+       
+       deallocate ( type_inp % rtm % rad_ch31_bb_prof )
+         if ( allocated( type_inp % rtm % rad_ch27_bb_prof)) deallocate ( type_inp % rtm % rad_ch27_bb_prof )
+       deallocate ( type_inp % rtm % t_prof )
+       deallocate ( type_inp % rtm % z_prof )
+   
+   end subroutine DEALLOCATE_INP
+
+end module cloud_type_bridge_module
---
+++ branches/forward_scattering/main_src/goestran.f
@@ -0,0 +1,186 @@
+c--------------------------------------------------------------------------
+c Clouds from AVHRR Extended (CLAVR-x) 1b PROCESSING SOFTWARE Version 5.3
+c
+c NAME: goestran.f (src)
+c
+c PURPOSE: GOES/I-N 101-level dry/wet/ozo fast transmittance calculation
+c
+c DESCRIPTION: 
+c      * PFAAST (formerly PLOD) regression model based on
+c      *     LBLRTM line-by-line transmittances.
+c      * Input temperatures, and water-vapor and ozone mixing ratios, must
+c      *     be defined at the 101 levels in array 'pstd' (see block
+c      *     data 'reference_atmosphere' below).
+c      * Logical unit numbers 71-75 are used for coefficient files.
+c      * Component taus are returned through common, product in 'taut'.
+c
+c AUTHORS:
+c  Andrew Heidinger, Andrew.Heidinger@noaa.gov
+c  Andi Walther, CIMSS, andi.walther@ssec.wisc.edu
+c  Denis Botambekov, CIMSS, denis.botambekov@ssec.wisc.edu
+c  William Straka, CIMSS, wstraka@ssec.wisc.edu
+c
+c COPYRIGHT
+c THIS SOFTWARE AND ITS DOCUMENTATION ARE CONSIDERED TO BE IN THE PUBLIC
+c DOMAIN AND THUS ARE AVAILABLE FOR UNRESTRICTED PUBLIC USE. THEY ARE
+c FURNISHED "AS IS." THE AUTHORS, THE UNITED STATES GOVERNMENT, ITS
+c INSTRUMENTALITIES, OFFICERS, EMPLOYEES, AND AGENTS MAKE NO WARRANTY,
+c EXPRESS OR IMPLIED, AS TO THE USEFULNESS OF THE SOFTWARE AND
+c DOCUMENTATION FOR ANY PURPOSE. THEY ASSUME NO RESPONSIBILITY (1) FOR
+c THE USE OF THE SOFTWARE AND DOCUMENTATION; OR (2) TO PROVIDE TECHNICAL
+c SUPPORT TO USERS.
+c
+c * Input
+c        temp = profile of temperature ........ degK
+c        wvmr = profile of H2O mixing ratio ... g/kg
+c        ozmr = profile of  O3 mixing ratio ... ppmv
+c       theta = local zenith angle ............ deg
+c        kban = band or channel number ........ see below
+c       ngoes = satellite number .............. 8 - 12
+c * Output
+c        taut = profile of total transmittance (components are returned through 
+c               common)
+c           * = error return in case of coefficient-file I/O trouble
+c * Band (or channel) numbering scheme:
+c       Sounder  1 - 18 are  1 - 18 in the context of this routine.
+c       Imager   2 - 6  are 22 - 26 in the context of this routine.
+c                for GOES-I through L: 22-25
+c                for GOES-M and later: 22-24,26
+c--------------------------------------------------------------------------
+
+        subroutine goestran(ancil_data_path,temp,wvmr,ozmr,theta, 
+     &                        ngoes,kban,taut,*)
+
+! RCS Version: $Id:
+
+c .... version of 15.02.06
+
+        parameter (lfac=4,nk=5,nl=101,nm=nl-1,nr=23)
+        parameter (nxc= 4,ncc=nxc+1,lencc=ncc*nm,lenccb=lencc*lfac)
+        parameter (nxd= 8,ncd=nxd+1,lencd=ncd*nm,lencdb=lencd*lfac)
+        parameter (nxo= 9,nco=nxo+1,lenco=nco*nm,lencob=lenco*lfac)
+        parameter (nxl= 2,ncl=nxl+1,lencl=ncl*nm,lenclb=lencl*lfac)
+        parameter (nxs=11,ncs=nxs+1,lencs=ncs*nm,lencsb=lencs*lfac)
+        parameter (nxw=nxl+nxs)
+        common/stdatm/pstd(nl),tstd(nl),wstd(nl),ostd(nl)
+        common/taudwo/taud(nl),tauw(nl),tauo(nl)
+        dimension temp(*),wvmr(*),ozmr(*),taut(*)
+        dimension coefd(ncd,nm,nr),coefo(nco,nm,nr),coefl(ncl,nm,nr)
+        dimension coefs(ncs,nm,nr),coefc(ncc,nm,nr),iuc(nk)
+        dimension pavg(nm),tref(nm),wref(nm),oref(nm)
+        dimension tavg(nm),wamt(nm),oamt(nm),secz(nm)
+        dimension tauc(nl),tlas(nl),wlas(nl),olas(nl)
+        dimension xdry(nxd,nm),xozo(nxo,nm),xwet(nxw,nm),xcon(nxc,nm)
+        character*32 cfile
+        character*72 ancil_data_path,pfaast_path
+        character*3 comp(nk)
+        data comp /'dry','ozo','wts','wtl','wco'/
+        integer*4 lencf(nk)
+        data lencf /lencdb,lencob,lencsb,lenclb,lenccb/
+        logical newang,newatm
+        integer*4 get_lun
+        data lgoes/0/,tlas/nl*0./,wlas/nl*0./,olas/nl*0./,zlas/-999./
+        secant(z)=1./cos(0.01745329*z)
+
+        cfile = 'goesccom.dat'
+        pfaast_path = trim(ancil_data_path)//"static/pfaast/"
+
+        if(ngoes.ne.lgoes) then
+         ! iux=70             !akh commented 
+           do l=1,nk
+              cfile(6:8)=comp(l)
+             !iux=iux+1       !akh commented
+              iux = get_lun()    !akh added
+              open(iux,file=trim(pfaast_path)//cfile,recl=lencf(l),
+     +                  access='direct',status='old',
+     +                  err=200)
+              iuc(l)=iux
+           enddo
+           koff=(ngoes-8)*nr
+           do k=1,nr
+              krec=k+koff
+              read(iuc(1),rec=krec) ((coefd(i,j,k),i=1,ncd),j=1,nm)
+              read(iuc(2),rec=krec) ((coefo(i,j,k),i=1,nco),j=1,nm)
+              read(iuc(3),rec=krec) ((coefs(i,j,k),i=1,ncs),j=1,nm)
+              read(iuc(4),rec=krec) ((coefl(i,j,k),i=1,ncl),j=1,nm)
+              read(iuc(5),rec=krec) ((coefc(i,j,k),i=1,ncc),j=1,nm)
+           enddo
+           do l=1,nk
+              close(iuc(l))
+           enddo
+           call conpir(pstd,tstd,wstd,ostd,nl,1,pavg,tref,wref,oref)
+           lgoes=ngoes
+        endif
+
+        do j=1,nl
+           taud(j)=1.0
+           tauw(j)=1.0
+           tauc(j)=1.0
+           tauo(j)=1.0
+           taut(j)=1.0
+        enddo
+c ## No transmittance calculations for visible bands!
+        if(kban.gt.18.and.kban.lt.22) return
+c ## Following allows for IMAGER band variations
+        if(ngoes.ge.12) then
+           if(kban == 25) return      ! there is no band 5 on GOES-12+
+        else
+           if(kban == 26) return      ! there is no band 6 on GOES-8 to 11
+        endif
+
+        dt=0.
+        dw=0.
+        do=0.
+        do j=1,nl
+           dt=dt+abs(temp(j)-tlas(j))
+           tlas(j)=temp(j)
+           dw=dw+abs(wvmr(j)-wlas(j))
+           wlas(j)=wvmr(j)
+           do=do+abs(ozmr(j)-olas(j))
+           olas(j)=ozmr(j)
+        enddo
+        datm=dt+dw+do
+        newatm=datm.ne.0.
+
+        if(newatm) then
+           call conpir(pstd,temp,wvmr,ozmr,nl,1,pavg,tavg,wamt,oamt)
+        endif
+
+        newang=theta.ne.zlas
+        if(newang) then
+           zsec=secant(theta)
+           do l=1,nm
+              secz(l)=zsec
+           enddo
+           zlas=theta
+        endif
+
+        if(newang.or.newatm) then
+           call calpir(tref,wref,oref,tavg,wamt,oamt,pavg,secz,
+     +                   nm,nxd,nxw,nxo,nxc,xdry,xwet,xozo,xcon)
+        endif
+
+        k=kban
+        if(kban.gt.18) k=k-3
+
+c * dry
+        call taudoc(ncd,nxd,nm,coefd(1,1,k),xdry,taud)
+
+c * ozo
+        call taudoc(nco,nxo,nm,coefo(1,1,k),xozo,tauo)
+
+c * wet
+        call tauwtr(ncs,ncl,nxs,nxl,nxw,nm,coefs(1,1,k),
+     +                  coefl(1,1,k),xwet,tauw)
+        call taudoc(ncc,nxc,nm,coefc(1,1,k),xcon,tauc)
+        do j=1,nl
+           tauw(j)=tauw(j)*tauc(j)
+        enddo
+
+c * total
+        do j=1,nl
+           taut(j)=taud(j)*tauo(j)*tauw(j)
+        enddo
+        return
+200     return1
+        end

0b38b18fe4e073b86d51ee911f287b65d96ddd50
awalther <awalther@98759d60-67cc-457f-8aaf-b3b51597e956>
2016-11-02 16:17:16 +0000

    add makefile

---
+++ CX_DATA_IO/cx_sds_write_mod.F90
@@ -0,0 +1,16 @@
+! $Id$
+module cx_sds_write_mod
+
+contains
+   function cx_sds_fcreate
+   
+   
+   end function cx_file_create
+   
+   function cx_sds_create
+   
+   end function cx_sds_create
+   
+   
+
+end module cx_sds_write_mod

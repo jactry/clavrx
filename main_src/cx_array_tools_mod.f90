562da6eea87bcadca7cab2fb44fe3647acecfb3d
awalther <awalther@98759d60-67cc-457f-8aaf-b3b51597e956>
2017-01-05 19:07:40 +0000

    forther logical/integer

--- main_src/cx_array_tools_mod.f90
+++ main_src/cx_array_tools_mod.f90
@@ -8,7 +8,7 @@ module cx_array_tools_mod
    implicit none
    
    interface cx_rebin
-      module procedure cx_rebin_real, cx_rebin_i1
+      module procedure cx_rebin_real, cx_rebin_i1 , cx_rebin_lo
    end interface
 
 
@@ -58,5 +58,27 @@ contains
       end do   
    
    
-   end function cx_rebin_i1
+   end function cx_rebin_lo
+   
+   function cx_rebin_i1 ( array, d1, d2 )
+      logical, intent(in) :: array(:,:)
+      integer, intent(in) :: d1, d2
+      logical, allocatable :: cx_rebin_lo(:,:)
+      
+      integer :: dim_array (2)
+      integer :: i, j
+      
+      dim_array = shape ( array)
+      allocate (cx_rebin_i1(d1,d2))
+      
+      do i = 1 , dim_array(1)
+         do j = 1, d2/2
+            cx_rebin_i1(2*i-1 : 2*i, 2*j-1 : 2*j) = array(i,j)
+         
+         end do
+      end do   
+   
+   
+   end function cx_rebin_lo
+   
 end module cx_array_tools_mod

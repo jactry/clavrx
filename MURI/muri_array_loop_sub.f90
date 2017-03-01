7e7ea50d0f0a9625a960a6b368a16925c2bae461
awalther <awalther@98759d60-67cc-457f-8aaf-b3b51597e956>
2017-03-01 11:31:34 +0000

    up

---
+++ MURI/muri_array_loop_sub.f90
@@ -0,0 +1,21 @@
+!
+!
+!
+
+subroutine muri_array_loop (input, output )
+   
+   use muri_defintions_mod
+   
+   implicit none
+   
+   type ( muri_in_array_type) :: input
+   type ( muri_out_array_type) :: output
+   
+
+
+
+
+
+
+
+end subroutine muri_array_loop

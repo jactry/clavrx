318b8c587d5bffd25e6bc70092c28f67b29738ad
awalther <awalther@98759d60-67cc-457f-8aaf-b3b51597e956>
2017-03-01 09:58:31 +0000

    up

--- MURI/muri_one_pixel_run.f90
+++ MURI/muri_one_pixel_run.f90
@@ -12,7 +12,19 @@ program one_pixel_run
    implicit none   
    type ( muri_input_type) :: inp
    type ( muri_output_type) :: out
-   inp % rfl(1) = 0.3   
+   
+   inp % rfl(1) = 0.3 
+   inp % rfl(2) = 0.1
+   inp % rfl(3) = 0.2
+   inp % rfl(4) = 0.122
+   inp % rfl(5) = 0.33
+   inp % rfl(6) = 0.11
+   
+   inp % sol = 22.
+   inp % sat = 13.
+   inp % azi = 120.
+   
+   call inp % info  
    
    call muri_algorithm (inp,out)
    

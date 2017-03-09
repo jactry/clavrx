1e62838a6a502fdb6a014f72c7295953b9d29637
awalther <awalther@98759d60-67cc-457f-8aaf-b3b51597e956>
2017-03-09 14:49:23 +0000

    add

---
+++ branches/dcomp_all_modes/MURI/muri_interface_mod.f90
@@ -0,0 +1,17 @@
+! $Id$
+!
+!
+module muri_interface_mod
+
+   type muri_in_array_type
+      real :: sol
+   
+   end type muri_in_array_type
+   
+   type muri_out_array_type
+      real :: aod
+   
+   end type muri_out_array_type
+   
+
+end module muri_interface_mod

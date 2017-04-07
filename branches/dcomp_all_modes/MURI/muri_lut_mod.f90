d2e98cefdcbda8ed095fde9baccbcd0e9ad7cc1c
awalther <awalther@98759d60-67cc-457f-8aaf-b3b51597e956>
2017-04-07 23:19:30 +0000

    update

---
+++ branches/dcomp_all_modes/MURI/muri_lut_mod.f90
@@ -0,0 +1,91 @@
+! $Id$
+
+
+module muri_lut_mod
+   use cx_sds_type_definitions_mod
+   
+   use cx_sds_io_mod, only: &
+      cx_sds_finfo &
+      , cx_sds_read
+   
+   implicit none 
+   type muri_lut_type
+      real, allocatable :: sol(:,:)
+      real, allocatable :: sat(:,:)
+      real, allocatable :: azi(:,:)
+      real, allocatable :: ws(:,:)
+      real  :: aot_550nm (8)
+      real, allocatable :: app_refl(:,:,:,:,:,:,:)
+      real, allocatable :: aot_aer(:,:,:,:,:,:,:) 
+      
+      real :: aot_aer_fine (8,6,4)
+      real :: aot_aer_coarse (8,6,5)
+      real :: refl_fine (8,6,4)
+      real :: refl_coarse (8,6,5)
+      
+      contains
+      procedure :: read_lut
+     
+     
+   end type muri_lut_type
+
+contains
+  !
+   !
+   !
+   subroutine read_lut(this)
+      class(muri_lut_type ) :: this
+      
+      character (len =400)::lut_file
+      integer :: istatus
+      integer :: ftype
+      integer :: nsds
+      integer :: natt
+      character ( len = MAXNCNAM), allocatable :: sds_name(:)
+      character ( len = MAXNCNAM), allocatable :: att_name(:)
+      real,allocatable :: sol_zen_ang(:,:)
+      real,allocatable :: sat_zen_ang(:,:)
+      logical :: file_exists
+      real, allocatable :: temp_2d_real(:,:)
+      lut_file = trim('/DATA/AHI_AEROSOL/AHI_Aerosol_LUT/AHI_Ocean_Aerosol_LUT_v1.hdf')
+      INQUIRE(file = lut_file,EXIST=file_exists)
+      if ( .not. file_exists) then 
+         print*,'LUT file not there stopping'
+         stop
+      
+      end if
+      istatus = cx_sds_finfo ( trim(lut_file), ftype, nsds,sds_name, natt,att_name)
+     
+      istatus = cx_sds_read ( trim(lut_file),'Solar_Zenith_Angles',this % sol)
+      istatus = cx_sds_read ( trim(lut_file),'View_Zenith_Angles', this % sat)
+      istatus = cx_sds_read ( trim(lut_file),'Relative_Azimuth_Angles', this % azi)
+      istatus = cx_sds_read ( trim(lut_file),'Wind_Speed', this % ws)
+      istatus = cx_sds_read ( trim(lut_file),'AOT_at_550nm',temp_2d_real)
+      this % aot_550nm(:) = temp_2d_real (1,:)
+      istatus = cx_sds_read( trim(lut_file), 'Apparent_Reflectance', this%app_refl)
+     
+      istatus = cx_sds_read ( trim(lut_file),'Aer_AOT_total', this % aot_aer )
+      
+      
+      ! - this is okay 
+      this % aot_aer_fine =   this % aot_aer(1,1,1,:,:,1,1:4)
+      this % aot_aer_coarse = this % aot_aer(1,1,1,:,:,1,5:9)
+      
+      !TODO this has to be interpolated
+      this % refl_fine =   this % app_refl(1,1,1,:,:,1,1:4)
+      this % refl_coarse = this % app_refl(1,1,1,:,:,1,5:9)
+     
+      
+   end subroutine read_lut
+   
+  
+   
+
+
+
+end module muri_lut_mod
+
+
+
+
+
--- branches/dcomp_all_modes/MURI/muri_retrieval_mod.f90
+++ branches/dcomp_all_modes/MURI/muri_retrieval_mod.f90
@@ -7,43 +7,140 @@ module muri_retrieval_mod
    
    use muri_forward_mod, only: &
       muri_fwd_type &
-      , muri_forward
+      , muri_forward &
+      ,  muri_lut_type
+   
+   use lib_array
       
    implicit none
    private
    public :: muri_algorithm 
 contains
+
+   !
+   !
+   !
    subroutine muri_algorithm (inp, out)
       type( muri_input_type), intent(in) :: inp
       type( muri_output_type), intent(out) :: out
       
       type ( muri_fwd_type ) :: fwd
-      
-      
+      type ( muri_lut_type)  :: lut
+      integer :: i_fm, i_cm
+      real :: fmr
+      real :: refl_toa (8,11,6)
+      real :: aot_mixed  (8,11,6)
+      integer :: i_cha,i_opt, i_fmr
+      real :: refl_ch4 (8)
+      real :: aot_temp(11)
+      real :: refl_corrsp(11,6)
+      real :: temp(8)
+      real :: aot_allbands(11,6)
+      real :: n1 (11)
+      real :: n2
+      real :: err(11,6)
+      real :: err_sqrt (11)
+      real :: aod_nleta_temp(4,5)
+      real :: err_nleta_temp(4,5)
+      real :: fmf_nleta_temp(4,5)
+      integer :: idx(1), idx2(2)
+      real :: val,val2
+      real :: aod_allbands(4,5,6)
       print*,'start muri algorithm'
       call inp%info
       
       
-      ! - first prepare input
-      ! - - done
+      call lut % read_lut
+      
+      ! - loop over fine and coarse mode
+      do i_fm = 1, 4
+         do i_cm = 1, 5
+            
+            ! loop over fine mode ratio
+            do i_fmr = 0,10
+               fmr = i_fmr/10.
+               
+               do i_opt = 1, 8
+                  do i_cha = 1,6
+                     refl_toa (i_opt,i_fmr,i_cha) = fmr * lut % refl_fine(i_opt,i_cha,i_fm) &
+                        & + (1 - fmr) * lut % refl_coarse(i_opt,i_cha,i_cm)
+                     aot_mixed(i_opt,i_fmr,i_cha) = fmr * lut % aot_aer_fine(i_opt,i_cha,i_fm) &
+                        & + (1 - fmr) * lut % aot_aer_coarse(i_opt,i_cha,i_cm)   
+                     
+                  end do
+               end do  
+               
+               !- reference channel is #4
+               refl_ch4 = refl_toa(:,i_fmr,4)
+               
+               
+               aot_temp(i_fmr) = interp1d(refl_ch4,lut % aot_550nm, inp % rfl(4),bounds_error = .false., FILL_VALUE = -999.)
+               
+               
+               do i_cha = 1,6
+                 temp =  aot_mixed(:,i_fmr,i_cha)
+                 aot_allbands (i_fmr,i_cha) = interp1d(refl_ch4,temp,inp % rfl(4),bounds_error = .false., FILL_VALUE = -999.)
+               end do
+               
+                  
+            end do
+            
+             do i_cha = 1,6
+                  do i_fmr =1,11
+                     refl_corrsp(i_fmr,i_cha) = interp1d(lut % aot_550nm,refl_toa(:,i_fmr,i_cha), aot_temp(i_fmr),bounds_error = .false., FILL_VALUE = -999. )
+                     
+                  end do
+               end do
+               
+            
+            do i_cha = 1,6
+               !print*,i_cha,refl_corrsp(:,i_cha)
+               n1 = inp % rfl(i_cha) - refl_corrsp(:,i_cha)
+               n2 = inp % rfl(i_cha) - refl_toa(1,1,i_cha) + 0.001
+               
+               
+             
+               
+               
+               
+               err(:,i_cha) = n1/n2
+               
+            end do
+           
+            err_sqrt = sqrt ( (err(:,3)**2. + err(:,4)**2.+ err(:,5)**2.+ err(:,6)**2.)/4.)  
+            
+            
+            val= minval(err_sqrt)
+            idx=minloc(err_sqrt)
+            aod_nleta_temp(i_fm,i_cm) = aot_temp(idx(1))
+            err_nleta_temp(i_fm,i_cm) = val
+            fmf_nleta_temp(i_fm,i_cm) = (idx(1)) /10.
+            
+            aod_allbands (i_fm,i_cm,:) = aot_allbands(idx(1),:)
+            
+            print*,i_fm,i_cm,    aod_nleta_temp(i_fm,i_cm) ,val,fmf_nleta_temp(i_fm,i_cm)     
+            
+         end do
+      end do
       
       
-      ! - now prepare the table
+     val2= minval(err_nleta_temp)
+     idx2=minloc(err_nleta_temp)
       
       
+      print*,idx2
       
-      call muri_forward ( 4.5, fwd)
       
-      
-      ! - do something with forward model
       
       
       
       
        !- once find a good match between fwd and measurment give output
-      out% aot = 3.4
+      out% aot = aod_nleta_temp(idx2(1),idx2(2))
       
-      
+      do i_cha =1,6 
+         out% aot_channel(i_cha) = aod_allbands(idx2(1),idx2(2),i_cha)
+      end do
       
    
    
